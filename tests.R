b97628d7c4ebcc72b449eb61836f10d1c709dc11
oli <o@o1o.ch>
2015-09-09 15:44:35 +0200

    detect stale package installation when testing as lib

--- tests.R
+++ tests.R
@@ -1,3 +1,10 @@
+if("rjit" %in% rownames(installed.packages())) {
+    print("*****************")
+    print("You have an existing version of rjit installed.")
+    print("Please remove first (i.e. make clean) to avoid unexpected results!")
+    exit()
+}
+
 dyn.load("librjit.so")
 source("rjit/R/rjit.R")
 library(compiler)
