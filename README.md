69ca1dcf4c84b0f6c72c793bc7060856724ab279
oli <o@o1o.ch>
2015-09-21 23:15:15 +0200

    make setup script re-runnable and update readme

--- README.md
+++ README.md
@@ -2,51 +2,43 @@
 
 # Installation
 
-TL;DR - complete script to get everything needed is available at [Etherpad](https://etherpad.mozilla.org/aXN4g6hiTW). Subsections are meant to be possibly clear out some details about [buildin llvm](#building-llvm), [buildin GNUR and rjit](#building-modified-gnur-and-rjit) and [running tests](#running-tests). 
+TL;DR - complete script to get everything needed is
+```
+tools/setup.sh ..
+```
+
+To install llvm to a different place
+```
+tools/setup.sh some/source/folder
+```
+
+To build off tree
+```
+mkdir build && cd build
+../tools/setup.sh ../..
+```
+
+To build using ninja
+```
+tools/setup.sh .. ninja
+```
+
+Subsections are meant to be possibly clear out some details about [buildin llvm](#building-llvm), [buildin GNUR and rjit](#building-modified-gnur-and-rjit) and [running tests](#running-tests). 
 
 ## Building llvm
 
-LLVM is used for compiling R bytecode and we will build it from the source code (it should be possible to use LLVM installed by package manager, but that has not been tested). 
+LLVM is used for compiling R bytecode and we will build it from the source code (it should be possible to use LLVM 3.7.0 installed by package manager, but that has not been tested). 
 
-```bash
-svn co https://llvm.org/svn/llvm-project/llvm/tags/RELEASE_370/final/ llvm-src
-mkdir llvm-build
-cd llvm-build
-cmake -G 'Unix Makefiles' -DLLVM_ENABLE_RTTI=1 --enable-debug-symbols --with-oprofile ../llvm-src
-make -j 8
-cd ../
-```
+"tools/setup.sh dir" will install and build llvm in dir/llvm 
 
 ## Building modified GNUR and rjit
 
 Running `rjit` requres a modified version of [GNU-R](https://bitbucket.org/reactorl/gnur), so next step is to build it from source. Note that on Mac OSX, you will need to install `fortan` compiler separately from [here](https://gcc.gnu.org/wiki/GFortranBinaries#MacOS) or using [brew](http://brew.sh/) `brew install gcc` (which will also install `gfortran`).
-
-```bash
-git clone https://bitbucket.org/reactorl/rjit.git
-cd rjit
-git clone https://bitbucket.org/reactorl/gnur
-cd gnur
-git checkout rllvm
-# configure and build
-if [[ "$OSTYPE" == "darwin"* ]]; then
-# Mac OSX
-        F77="gfortran -arch x86_64" FC="gfortran -arch x86_64" CPPFLAGS="-O0 -gdwarf-2 -g3 -arch x86_64" CFLAGS="-O0 -gdwarf-2 -g3 -arch x86_64" ./configure --with-blas --with-lapack --with-ICU=no --with-system-xz=no --with-system-zlib=no --with-x=no --with-readline=no --without-recommended-packages
-else
-        CPPFLAGS="-O0 -gdwarf-2 -g3" CFLAGS="-O0 -gdwarf-2 -g3" ./configure --with-blas --with-lapack --with-ICU=no --with-system-xz=no --with-system-zlib=no --with-x=no --with-readline=no --without-recommended-packages
-fi
-sed -i -e "s/exit 1/\$(ECHO) \"I am feeling lucky\"/g" Makefile
-make -j 8
-
-cd ..
-# build rjit
-cmake -DLLVM_DIR=../llvm-build/share/llvm/cmake .
-make
-```
 
 ## Running tests
 
 To run the basic test, just execute:
 
 ```
-gnur/bin/R -f tests.R
+make tests
 ```
--- tools/setup.sh
+++ tools/setup.sh
@@ -3,6 +3,9 @@
 set -e
 
 TARGET=$1
+OPT="-O0"
+
+BUILD_DIR=`pwd`
 
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
@@ -12,36 +15,48 @@ if [ -z $TARGET ]; then
   echo "usage: ./${0} target_directory [ninja]"
   exit 1
 fi
+
+LLVM_TARGET=${TARGET}/llvm
+
 if [ ! -d $TARGET ]; then
   echo "-> creating ${TARGET}"
   mkdir $TARGET
 fi
+
+CORES=`nproc || echo 8`
 
 if [[ $2 == "ninja" ]]; then
     GEN="Ninja"
     M="ninja"
 else
     GEN="Unix Makefiles"
-    M="make -j8"
+    M="make -j${CORES}"
 fi
 
 LLVM_VERS="370"
 
-
-LLVM_TARGET=${TARGET}/llvm
-mkdir ${LLVM_TARGET}
+if [ ! -d ${LLVM_TARGET} ]; then
+    mkdir ${LLVM_TARGET}
+fi
 cd ${LLVM_TARGET}
 
 LLVM_SRC_F=llvm-src-${LLVM_VERS}
 LLVM_SRC=${LLVM_TARGET}/${LLVM_SRC_F}
-echo "-> checking out llvm ${LLVM_VERS} to ${LLVM_SRC}"
 
-svn co http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_${LLVM_VERS}/final/ llvm-src-${LLVM_VERS}
+if [ ! -d ${LLVM_SRC} ]; then
+    echo "-> checking out llvm ${LLVM_VERS} to ${LLVM_SRC}"
+
+    svn co http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_${LLVM_VERS}/final/ llvm-src-${LLVM_VERS}
+else
+    echo "-> svn revert ${LLVM_SRC}"
+    cd ${LLVM_SRC}
+    svn revert .
+fi
 
 LLVM_BUILD_DIR_F=llvm-build-${LLVM_VERS}
 LLVM_BUILD_DIR=${LLVM_TARGET}/${LLVM_BUILD_DIR_F}
 echo "-> building llvm to ${LLVM_BUILD_DIR}"
-mkdir $LLVM_BUILD_DIR
+mkdir -p $LLVM_BUILD_DIR
 cd $LLVM_BUILD_DIR
 cmake -G "$GEN" -DLLVM_ENABLE_RTTI=1 --enable-debug-symbols --with-oprofile ${LLVM_SRC}
 $M
@@ -49,33 +64,36 @@ $M
 cd $SRC_DIR
 
 if [ ! -d gnur ]; then
-  echo "-> checking out gnur" 
-  git clone https://bitbucket.org/reactorl/gnur
-  cd gnur
-  git checkout rllvm
+    echo "-> checking out gnur" 
+    git clone https://bitbucket.org/reactorl/gnur
 fi
+
+echo "-> git reset gnur to rllvm" 
+cd gnur
+git checkout rllvm
 
 echo "-> configure gnur"
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # Mac OSX
-  F77="gfortran -arch x86_64" FC="gfortran -arch x86_64" CXXFLAGS="-fno-omit-frame-pointer -gdwarf-2 -g3 -arch x86_64" CFLAGS="-fno-omit-frame-pointer -gdwarf-2 -g3 -arch x86_64" ./configure --with-blas --with-lapack --with-ICU=no --with-system-xz=no --with-system-zlib=no --with-x=no --with-readline=no --without-recommended-packages
+  F77="gfortran -arch x86_64" FC="gfortran -arch x86_64" CXXFLAGS="$OPT -fno-omit-frame-pointer -gdwarf-2 -g3 -arch x86_64" CFLAGS="$OPT -fno-omit-frame-pointer -gdwarf-2 -g3 -arch x86_64" ./configure --with-blas --with-lapack --with-ICU=no --with-system-xz=no --with-system-zlib=no --with-x=no --with-readline=no --without-recommended-packages
 else
-    CXXFLAGS="-fno-omit-frame-pointer -gdwarf-2 -g3" CFLAGS="-fno-omit-frame-pointer -gdwarf-2 -g3" ./configure --with-blas --with-lapack --with-ICU=no --with-system-xz=no --with-system-zlib=no --with-x=no --with-readline=no --without-recommended-packages
+    CXXFLAGS="$OPT -fno-omit-frame-pointer -gdwarf-2 -g3" CFLAGS="$OPT -fno-omit-frame-pointer -gdwarf-2 -g3" ./configure --with-blas --with-lapack --with-ICU=no --with-system-xz=no --with-system-zlib=no --with-x=no --with-readline=no --without-recommended-packages
 fi
-sed -i -e "s/exit 1/\$(ECHO) \"I am feeling lucky\"/g" Makefile
+#sed -i -e "s/exit 1/\$(ECHO) \"I am feeling lucky\"/g" Makefile
 echo "Revision: -99" > SVN-REVISION
-rm non-tarball
+rm -f non-tarball
 
 echo "-> build gnur"
-make -j 8
+make -j${CORES}
 
-cd ..
+cd $BUILD_DIR
 echo "-> cmake rjit"
-cmake -G "$GEN" -DLLVM_DIR=${LLVM_BUILD_DIR}/share/llvm/cmake .
+rm -f CMakeCache.txt
+cmake -G "$GEN" -DLLVM_DIR=${LLVM_BUILD_DIR}/share/llvm/cmake $SRC_DIR
 $M
 
 echo "-> install hooks"
-tools/install_hooks.sh
+${SRC_DIR}/tools/install_hooks.sh
 
 echo "-> running tests"
-tools/tests
+${SRC_DIR}/tools/tests
