fa19c4be0b1adf1822a0b32dd5c32f32f16e8e1c
peta <peta.maj82@gmail.com>
2015-09-29 21:32:48 +0200

    More functionality added to the builder.

--- rjit/src/ir/ir.cpp
+++ rjit/src/ir/ir.cpp
@@ -1,11 +1,36 @@
 #include "ir.h"
 
+using namespace llvm;
+
 namespace rjit {
 namespace ir {
 
+
+// TODO change CBR to match compare and branch
+Type Instruction::match(BasicBlock::iterator & i) {
+    llvm::Instruction * ins = i;
+    ++i; // move to next instruction
+    if (isa<CallInst>(ins)) {
+        Type t = Intrinsic::getIRType(ins);
+        if (t != Type::unknown) {
+            return t;
+        }
+    } else if (isa<ReturnInst>(ins)) {
+        return Type::ret;
+    } else if (isa<BranchInst>(ins)) {
+        BranchInst * b = cast<BranchInst>(ins);
+        return b->isConditional() ? Type::cbr : Type::br;
+    } else if (isa<ICmpInst>(ins)) {
+        return Type::cmp;
+    }
+    return Type::unknown;
+}
+
+
+
+
+
 char const * const Intrinsic::MD_NAME = "r_ir_type";
-
-
 
 } // namespace ir
 
