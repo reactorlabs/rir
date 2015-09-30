1a0b4c742ae5ef13f7ab67d1e4a2df8afbe67715
peta <peta.maj82@gmail.com>
2015-09-30 14:20:36 +0200

    Intrinsics.h added to the project. irTypes removed (integrated into ir.h as these will no longer be autogenerated).

--- rjit/src/api.cpp
+++ rjit/src/api.cpp
@@ -10,6 +10,7 @@
 #include "ir/ir.h"
 #include "ir/Builder.h"
 #include "ir/intrinsics.h"
+#include "ir/Handler.h"
 
 using namespace rjit;
 
@@ -88,3 +89,5 @@ REXPORT SEXP jittest(SEXP expression) {
         std::cout << (int)ir::Instruction::match(i) << std::endl;
     return expression;
 }
+
+
