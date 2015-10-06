b7aeb6cddd373affaef6d666552d567bc1088ca1
oli <o@o1o.ch>
2015-10-07 00:08:42 +0200

    extend test

--- rjit/tests/branch.R
+++ rjit/tests/branch.R
@@ -14,6 +14,20 @@ stopifnot(f() == 3)
 stopifnot(f() == 3)
 
 f <- jit.compile(function(){
+  if (!1) 0 else 3
+})
+stopifnot(f() == 3)
+stopifnot(f() == 3)
+
+
+f <- jit.compile(function(){
+  if (!2L) 1 else 3
+})
+stopifnot(f() == 3)
+stopifnot(f() == 3)
+
+
+f <- jit.compile(function(){
     lf <- length(1:2)
     if(lf) 1L:lf
 })
@@ -21,3 +35,6 @@ stopifnot(length(f()) == 2)
 
 seq.default <- jit.compile(seq.default)
 stopifnot(length(seq(1:2)) == 2)
+stopifnot(length(seq(1:2)) == 2)
+stopifnot(length(seq(11:13)) == 3)
+stopifnot(length(seq(seq(1:2))) == 2)
