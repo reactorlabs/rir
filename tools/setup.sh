ef909bf6edb3b38c199e1f84749dd334b19a5f12
oli <o@o1o.ch>
2015-10-19 11:22:17 +0200

    unbreak setup

--- tools/setup.sh
+++ tools/setup.sh
@@ -122,11 +122,11 @@ if [ ! -d $TARGET ]; then
     mkdir $TARGET
 fi
 
-if [ $SKIP_LLVM -eq 0 ]; then
-    LLVM_TARGET=${TARGET}/llvm
-    LLVM_BUILD_DIR_F=llvm-build-${LLVM_VERS}
-    LLVM_BUILD_DIR=${LLVM_TARGET}/${LLVM_BUILD_DIR_F}
+LLVM_TARGET=${TARGET}/llvm
+LLVM_BUILD_DIR_F=llvm-build-${LLVM_VERS}
+LLVM_BUILD_DIR=${LLVM_TARGET}/${LLVM_BUILD_DIR_F}
 
+if [ $SKIP_LLVM -eq 0 ]; then
     if [ ! -d ${LLVM_TARGET} ]; then
         mkdir ${LLVM_TARGET}
     fi
