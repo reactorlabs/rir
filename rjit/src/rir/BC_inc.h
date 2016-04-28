8450014f00e71939a1aa371dbdc564da014a41eb
oli <o@o1o.ch>
2016-04-28 22:57:23 +0200

    add some documentation

--- rjit/src/rir/BC_inc.h
+++ rjit/src/rir/BC_inc.h
@@ -9,18 +9,55 @@
 namespace rjit {
 namespace rir {
 
+// ============================================================
+// ==== Bytecode Layout:
+//
+// [ uint8_t bytecode | optional immediate argument ]
+//
 enum class BC_t : uint8_t {
     invalid,
+
+    // Push a constant to the stack
+    // I: constant (via Pool)
+    // S: +1
     push,
+
+    // Function lookup
+    // I: symbol (via Pool)
+    // S: +1
     getfun,
+
+    // Variable lookup
+    // I: symbol (via Pool)
+    // S: +1
     getvar,
+
+    // Call function
+    // I: N - number of arguments
+    // S: -N
     call,
+
+    // Call function with named arguments
+    // I: list of names (via Pool)
+    // S: - #names
     call_name,
+
+    // Create a promise
+    // I: promise index
+    // S: +1
     mkprom,
+
+    // Create a closure
+    // I: closure index
+    // S: +1
     mkclosure,
+
     num_of
 };
 
+// ============================================================
+// ==== immediate argument types
+//
 typedef uint32_t pool_idx_t;
 typedef uint16_t fun_idx_t;
 typedef uint16_t num_args_t;
@@ -35,6 +72,13 @@ static constexpr size_t MAX_NUM_ARGS = 1L << (8 * sizeof(num_args_t));
 static constexpr size_t MAX_FUN_IDX = 1L << (8 * sizeof(fun_idx_t));
 static constexpr size_t MAX_POOL_IDX = 1L << (8 * sizeof(pool_idx_t));
 
+// ============================================================
+// ==== Creation and decoding of Bytecodes
+//
+// * Static factory functions to create an instance of a bytecode
+//   which can be pushed onto a CodeStream
+// * read and advance to read the next bytecode from an array
+//
 class CodeStream;
 class BC {
     BC(BC_t bc) : bc(bc), immediate({0}) {}
@@ -45,16 +89,18 @@ class BC {
     const immediate_t immediate;
 
     inline size_t size() const;
-
     void write(CodeStream& cs) const;
 
+    // Getters for the immediate arguments
     SEXP immediateConst();
     inline fun_idx_t immediateFunIdx() { return immediate.fun; }
     inline fun_idx_t immediateNumArgs() { return immediate.numArgs; }
 
+    // Decode BC from bytecode stream
     inline const static BC read(BC_t* pc);
     inline const static BC advance(BC_t** pc);
 
+    // Create a new BC instance
     inline const static BC call(num_args_t numArgs);
     inline const static BC call_name(SEXP names);
     inline const static BC push(SEXP constant);
--- rjit/src/rir/Compiler.cpp
+++ rjit/src/rir/Compiler.cpp
@@ -19,24 +19,39 @@ namespace {
 fun_idx_t compileExpression(Function& f, SEXP exp);
 void compileExpression(Function& f, CodeStream& cs, SEXP exp);
 
+// function application
 void compileCall(Function& f, CodeStream& cs, SEXP ast, SEXP fun, SEXP args) {
+    // application has the form:
+    // LHS ( ARGS )
+
+    // LHS can either be an identifier or an expression
     Match(fun) {
         Case(SYMSXP) { cs << BC::getfun(fun); }
         Else(compileExpression(f, cs, fun));
     }
 
+    // Process arguments:
+    // Arguments can be optionally named
     size_t numArgs = 0;
     RVector names;
     bool hasNames = false;
+
     for (auto arg = RList(args).begin(); arg != RList::end(); ++arg) {
         numArgs++;
+
+        // (1) Arguments are wrapped as Promises:
+        //     create a new Code object for the promise
         size_t prom = compileExpression(f, *arg);
+
+        // (2) remember if the argument had a name associated
         if (arg.hasTag()) {
             hasNames = true;
             names.append(arg.tag());
         } else {
             names.append(R_NilValue);
         }
+
+        // (3) insert promise creation instruction into current CodeStream
         cs << BC::mkprom(prom);
     }
     assert(numArgs < MAX_NUM_ARGS);
@@ -48,14 +63,20 @@ void compileCall(Function& f, CodeStream& cs, SEXP ast, SEXP fun, SEXP args) {
     }
 }
 
+// Lookup
 void compileGetvar(CodeStream& cs, SEXP name) { cs << BC::getvar(name); }
 
+// Constant
 void compileConst(CodeStream& cs, SEXP constant) { cs << BC::push(constant); }
 
 void compileExpression(Function& f, CodeStream& cs, SEXP exp) {
+    // Dispatch on the current type of AST node
     Match(exp) {
+        // Function application
         Case(LANGSXP, fun, args) { compileCall(f, cs, exp, fun, args); }
+        // Variable lookup
         Case(SYMSXP) { compileGetvar(cs, exp); }
+        // Constant
         Else(compileConst(cs, exp));
     }
 }
@@ -72,6 +93,7 @@ SEXP Compiler::finalize() {
 
     compileExpression(f, exp);
 
+    // TODO...
     size_t i = 0;
     std::cout << "===========\n";
     for (auto c : f.code) {
