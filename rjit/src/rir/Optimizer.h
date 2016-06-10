reactorlabs/rjit

Finally a working code editor class

rjit/src/rir/Optimizer.h
@@ -7,6 +7,8 @@
 #include "CodeStream.h"
 #include <cassert>
 
+#include "CodeEditor.h"
+
 namespace rjit {
 namespace rir {
 
@@ -16,180 +18,167 @@ namespace {
 // ==== Thats just a toy example of how to compile away a call to some specials
 //
 void optimize_(Function* fun, fun_idx_t idx);
-void optimize(CodeStream& cs, Function* fun, Code* cur);
+void optimize(CodeEditor& e, Function* fun);
 
-BC_t* doInlineIf(CodeStream& cs, Function* fun, Code* cur, BC_t* pc,
-                 BC_t* end) {
+void inlProm(CodeEditor::Cursor cur, Function* fun, fun_idx_t idx) {
+    CodeEditor ce(fun->code[idx], idx);
+    ce.normalizeReturn();
+    cur << ce;
+}
 
-    cs << BC::check_primitive(symbol::If);
-
-    BC bc = BC::advance(&pc);
-
+void doInlineBlock(CodeEditor::Cursor& cur, Function* fun) {
+    assert((*cur).bc == BC_t::getfun);
+    cur.remove();
+    BC bc = *cur;
     assert(bc.bc == BC_t::call);
+    cur.remove();
+
+    cur << BC::check_primitive(symbol::Parenthesis);
+
     fun_idx_t* args = bc.immediateCallArgs();
     num_args_t nargs = bc.immediateCallNargs();
 
-    Label trueBranch = cs.mkLabel();
-    Label nextBranch = cs.mkLabel();
-
-    optimize(cs, fun, fun->code[args[0]]);
-    cs << BC::to_bool() << BC::jmp_true(trueBranch);
-
-    if (nargs < 3) {
-        cs << BC::push(R_NilValue);
-    } else {
-        optimize(cs, fun, fun->code[args[2]]);
+    for (int i = 0; i < nargs; ++i) {
+        inlProm(cur, fun, args[i]);
+        if (i != nargs - 1)
+            cur << BC::pop();
     }
-    cs << BC::jmp(nextBranch);
-
-    cs << trueBranch;
-    optimize(cs, fun, fun->code[args[1]]);
-
-    cs << nextBranch;
-
-    return pc;
 }
 
-BC_t* doInlinePar(CodeStream& cs, Function* fun, Code* cur, BC_t* pc,
-                  BC_t* end) {
-
-    cs << BC::check_primitive(symbol::Parenthesis);
-
-    BC bc = BC::advance(&pc);
-
+void doInlineIf(CodeEditor::Cursor& cur, Function* fun) {
+    assert((*cur).bc == BC_t::getfun);
+    cur.remove();
+    BC bc = *cur;
     assert(bc.bc == BC_t::call);
+    cur.remove();
+
+    cur << BC::check_primitive(symbol::If);
+
+    fun_idx_t* args = bc.immediateCallArgs();
+    num_args_t nargs = bc.immediateCallNargs();
+
+    Label trueBranch = cur.mkLabel();
+    Label nextBranch = cur.mkLabel();
+
+    inlProm(cur, fun, args[0]);
+    cur << BC::to_bool() << BC::jmp_true(trueBranch);
+
+    if (nargs < 3) {
+        cur << BC::push(R_NilValue);
+    } else {
+        inlProm(cur, fun, args[2]);
+    }
+    cur << BC::jmp(nextBranch);
+
+    cur << BC::label(trueBranch);
+    inlProm(cur, fun, args[1]);
+
+    cur << BC::label(nextBranch);
+}
+
+void doInlinePar(CodeEditor::Cursor& cur, Function* fun) {
+    assert((*cur).bc == BC_t::getfun);
+    cur.remove();
+    BC bc = *cur;
+    assert(bc.bc == BC_t::call);
+    cur.remove();
+
+    cur << BC::check_primitive(symbol::Parenthesis);
+
     fun_idx_t* args = bc.immediateCallArgs();
     num_args_t nargs = bc.immediateCallNargs();
 
     assert(nargs == 1);
 
-    optimize(cs, fun, fun->code[args[0]]);
-
-    return pc;
+    inlProm(cur, fun, args[0]);
 }
 
-BC_t* doInlineBlock(CodeStream& cs, Function* fun, Code* cur, BC_t* pc,
-                    BC_t* end) {
-
-    cs << BC::check_primitive(symbol::Block);
-
-    BC bc = BC::advance(&pc);
-
+void doInlineAdd(CodeEditor::Cursor& cur, Function* fun) {
+    assert((*cur).bc == BC_t::getfun);
+    cur.remove();
+    BC bc = *cur;
     assert(bc.bc == BC_t::call);
-    fun_idx_t* args = bc.immediateCallArgs();
-    num_args_t nargs = bc.immediateCallNargs();
+    cur.remove();
 
-    for (int i = 0; i < nargs; ++i) {
-        optimize(cs, fun, fun->code[args[i]]);
-    }
+    cur << BC::check_primitive(symbol::Add);
 
-    return pc;
-}
-
-BC_t* doInlineSub(CodeStream& cs, Function* fun, Code* cur, BC_t* pc,
-                  BC_t* end) {
-
-    cs << BC::check_primitive(symbol::Sub);
-
-    BC bc = BC::advance(&pc);
-
-    assert(bc.bc == BC_t::call);
-    fun_idx_t* args = bc.immediateCallArgs();
-    num_args_t nargs = bc.immediateCallNargs();
-
-    if (nargs == 1) {
-        cs << BC::push(0.0);
-        optimize(cs, fun, fun->code[args[0]]);
-        cs << BC::sub();
-        return pc;
-    }
-    assert(nargs == 2);
-    optimize(cs, fun, fun->code[args[0]]);
-    optimize(cs, fun, fun->code[args[1]]);
-
-    cs << BC::sub();
-
-    return pc;
-}
-
-BC_t* doInlineLt(CodeStream& cs, Function* fun, Code* cur, BC_t* pc,
-                 BC_t* end) {
-
-    cs << BC::check_primitive(symbol::Lt);
-
-    BC bc = BC::advance(&pc);
-
-    assert(bc.bc == BC_t::call);
-    fun_idx_t* args = bc.immediateCallArgs();
-    num_args_t nargs = bc.immediateCallNargs();
-    assert(nargs == 2);
-
-    optimize(cs, fun, fun->code[args[0]]);
-    optimize(cs, fun, fun->code[args[1]]);
-
-    cs << BC::lt();
-
-    return pc;
-}
-
-BC_t* doInlineAdd(CodeStream& cs, Function* fun, Code* cur, BC_t* pc,
-                  BC_t* end) {
-
-    cs << BC::check_primitive(symbol::Add);
-
-    BC bc = BC::advance(&pc);
-
-    assert(bc.bc == BC_t::call);
     fun_idx_t* args = bc.immediateCallArgs();
     num_args_t nargs = bc.immediateCallNargs();
 
     assert(nargs == 2);
-    optimize(cs, fun, fun->code[args[0]]);
-    optimize(cs, fun, fun->code[args[1]]);
-
-    cs << BC::add();
-
-    return pc;
+    inlProm(cur, fun, args[0]);
+    inlProm(cur, fun, args[1]);
+    cur << BC::add();
 }
 
-void optimize(CodeStream& cs, Function* fun, Code* cur) {
-    BC_t* pc = cur->bc;
-    BC_t* end = (BC_t*)(uintptr_t)pc + cur->size;
+void doInlineSub(CodeEditor::Cursor& cur, Function* fun) {
+    assert((*cur).bc == BC_t::getfun);
+    cur.remove();
+    BC bc = *cur;
+    assert(bc.bc == BC_t::call);
+    cur.remove();
 
-    while (pc != end) {
-        BC bc = BC::advance(&pc);
+    cur << BC::check_primitive(symbol::Sub);
+
+    fun_idx_t* args = bc.immediateCallArgs();
+    num_args_t nargs = bc.immediateCallNargs();
+
+    assert(nargs == 2);
+    inlProm(cur, fun, args[0]);
+    inlProm(cur, fun, args[1]);
+    cur << BC::sub();
+}
+
+void doInlineLt(CodeEditor::Cursor& cur, Function* fun) {
+    assert((*cur).bc == BC_t::getfun);
+    cur.remove();
+    BC bc = *cur;
+    assert(bc.bc == BC_t::call);
+    cur.remove();
+
+    cur << BC::check_primitive(symbol::Lt);
+
+    fun_idx_t* args = bc.immediateCallArgs();
+    num_args_t nargs = bc.immediateCallNargs();
+
+    assert(nargs == 2);
+    inlProm(cur, fun, args[0]);
+    inlProm(cur, fun, args[1]);
+    cur << BC::lt();
+}
+
+void optimize(CodeEditor& e, Function* fun) {
+    for (auto cur = e.getCursor(); !cur.atEnd(); ++cur) {
+        BC bc = *cur;
 
         switch (bc.bc) {
         case BC_t::getfun:
             if (bc.immediateConst() == symbol::If) {
-                pc = doInlineIf(cs, fun, cur, pc, end);
+                doInlineIf(cur, fun);
                 continue;
             }
             if (bc.immediateConst() == symbol::Block) {
-                pc = doInlineBlock(cs, fun, cur, pc, end);
+                doInlineBlock(cur, fun);
                 continue;
             }
             if (bc.immediateConst() == symbol::Lt) {
-                pc = doInlineLt(cs, fun, cur, pc, end);
+                doInlineLt(cur, fun);
                 continue;
             }
             if (bc.immediateConst() == symbol::Add) {
-                pc = doInlineAdd(cs, fun, cur, pc, end);
+                doInlineAdd(cur, fun);
                 continue;
             }
             if (bc.immediateConst() == symbol::Sub) {
-                pc = doInlineSub(cs, fun, cur, pc, end);
+                doInlineSub(cur, fun);
                 continue;
             }
             if (bc.immediateConst() == symbol::Parenthesis) {
-                pc = doInlinePar(cs, fun, cur, pc, end);
+                doInlinePar(cur, fun);
                 continue;
             }
 
             break;
-
-        case BC_t::ret:
-            continue;
 
         case BC_t::call: {
             fun_idx_t* args = bc.immediateCallArgs();
@@ -203,30 +192,29 @@ void optimize(CodeStream& cs, Function* fun, Code* cur) {
         default:
             break;
         }
-
-        // TODO: this only works because the unoptimized bytecode has no
-        // jumps!! We really need a wrapper that lets us modify bytecode while
-        // preserving jump targets.
-        cs << bc;
     }
 }
 
 void optimize_(Function* fun, fun_idx_t idx) {
     Code* c = fun->code[idx];
-
-    CodeStream opt(c->ast, idx);
-    optimize(opt, fun, c);
-    opt << BC::ret();
-
-    Code* optCode = opt.toCode();
-    delete fun->code[idx];
-    fun->code[idx] = optCode;
+    c->print();
+    CodeEditor edit(c, idx);
+    edit.print();
+    optimize(edit, fun);
+    edit.print();
+    Code* opt = edit.toCode();
+    opt->print();
+    fun->code[idx] = opt;
+    delete c;
 }
 }
 
 class Optimizer {
   public:
-    static void optimize(Function* fun) { optimize_(fun, 0); }
+    static void optimize(Function* fun) {
+        for (int i = 0; i < 5; ++i)
+            optimize_(fun, 0);
+    }
 };
 
 } // rir

reactorlabs/rir

rjit/src/rir/Optimizer.h
@@ -6,12 +6,202 @@
 #include "CodeStream.h"
 #include <cassert>
 
+#include "CodeEditor.h"
+
 namespace rjit {
 namespace rir {
 
+namespace {
+
+// ============================================================================
+// ==== Thats just a toy example of how to compile away a call to some specials
+//
+void optimize_(Code* fun);
+void optimize(CodeEditor& e, Code* fun);
+
+void inlProm(CodeEditor::Cursor cur, Code* fun, fun_idx_t idx) {
+    CodeEditor ce(fun->children[idx]);
+    ce.normalizeReturn();
+    cur << ce;
+}
+
+void doInlineBlock(CodeEditor::Cursor& cur, Code* fun) {
+    assert((*cur).bc == BC_t::getfun);
+    cur.remove();
+    BC bc = *cur;
+    assert(bc.bc == BC_t::call);
+    cur.remove();
+
+    cur << BC::check_primitive(symbol::Block);
+
+    fun_idx_t* args = bc.immediateCallArgs();
+    num_args_t nargs = bc.immediateCallNargs();
+
+    for (int i = 0; i < nargs; ++i) {
+        inlProm(cur, fun, args[i]);
+        if (i != nargs - 1)
+            cur << BC::pop();
+    }
+}
+
+void doInlineIf(CodeEditor::Cursor& cur, Code* fun) {
+    assert((*cur).bc == BC_t::getfun);
+    cur.remove();
+    BC bc = *cur;
+    assert(bc.bc == BC_t::call);
+    cur.remove();
+
+    cur << BC::check_primitive(symbol::If);
+
+    fun_idx_t* args = bc.immediateCallArgs();
+    num_args_t nargs = bc.immediateCallNargs();
+
+    Label trueBranch = cur.mkLabel();
+    Label nextBranch = cur.mkLabel();
+
+    inlProm(cur, fun, args[0]);
+    cur << BC::to_bool() << BC::jmp_true(trueBranch);
+
+    if (nargs < 3) {
+        cur << BC::push(R_NilValue);
+    } else {
+        inlProm(cur, fun, args[2]);
+    }
+    cur << BC::jmp(nextBranch);
+
+    cur << BC::label(trueBranch);
+    inlProm(cur, fun, args[1]);
+
+    cur << BC::label(nextBranch);
+}
+
+void doInlinePar(CodeEditor::Cursor& cur, Code* fun) {
+    assert((*cur).bc == BC_t::getfun);
+    cur.remove();
+    BC bc = *cur;
+    assert(bc.bc == BC_t::call);
+    cur.remove();
+
+    cur << BC::check_primitive(symbol::Parenthesis);
+
+    fun_idx_t* args = bc.immediateCallArgs();
+    num_args_t nargs = bc.immediateCallNargs();
+
+    assert(nargs == 1);
+
+    inlProm(cur, fun, args[0]);
+}
+
+void doInlineAdd(CodeEditor::Cursor& cur, Code* fun) {
+    assert((*cur).bc == BC_t::getfun);
+    cur.remove();
+    BC bc = *cur;
+    assert(bc.bc == BC_t::call);
+    cur.remove();
+
+    cur << BC::check_primitive(symbol::Add);
+
+    fun_idx_t* args = bc.immediateCallArgs();
+    num_args_t nargs = bc.immediateCallNargs();
+
+    assert(nargs == 2);
+    inlProm(cur, fun, args[0]);
+    inlProm(cur, fun, args[1]);
+    cur << BC::add();
+}
+
+void doInlineSub(CodeEditor::Cursor& cur, Code* fun) {
+    assert((*cur).bc == BC_t::getfun);
+    cur.remove();
+    BC bc = *cur;
+    assert(bc.bc == BC_t::call);
+    cur.remove();
+
+    cur << BC::check_primitive(symbol::Sub);
+
+    fun_idx_t* args = bc.immediateCallArgs();
+    num_args_t nargs = bc.immediateCallNargs();
+
+    assert(nargs == 2);
+    inlProm(cur, fun, args[0]);
+    inlProm(cur, fun, args[1]);
+    cur << BC::sub();
+}
+
+void doInlineLt(CodeEditor::Cursor& cur, Code* fun) {
+    assert((*cur).bc == BC_t::getfun);
+    cur.remove();
+    BC bc = *cur;
+    assert(bc.bc == BC_t::call);
+    cur.remove();
+
+    cur << BC::check_primitive(symbol::Lt);
+
+    fun_idx_t* args = bc.immediateCallArgs();
+    num_args_t nargs = bc.immediateCallNargs();
+
+    assert(nargs == 2);
+    inlProm(cur, fun, args[0]);
+    inlProm(cur, fun, args[1]);
+    cur << BC::lt();
+}
+
+void optimize(CodeEditor& e, Code* fun) {
+    for (auto cur = e.getCursor(); !cur.atEnd(); ++cur) {
+        BC bc = *cur;
+
+        switch (bc.bc) {
+        case BC_t::getfun:
+            if (bc.immediateConst() == symbol::If) {
+                doInlineIf(cur, fun);
+                continue;
+            }
+            if (bc.immediateConst() == symbol::Block) {
+                doInlineBlock(cur, fun);
+                continue;
+            }
+            if (bc.immediateConst() == symbol::Lt) {
+                doInlineLt(cur, fun);
+                continue;
+            }
+            if (bc.immediateConst() == symbol::Add) {
+                doInlineAdd(cur, fun);
+                continue;
+            }
+            if (bc.immediateConst() == symbol::Sub) {
+                doInlineSub(cur, fun);
+                continue;
+            }
+            if (bc.immediateConst() == symbol::Parenthesis) {
+                doInlinePar(cur, fun);
+                continue;
+            }
+
+            break;
+
+        default:
+            break;
+        }
+    }
+}
+
+void optimize_(Code* fun) {
+    CodeEditor edit(fun);
+    optimize(edit, fun);
+    Code* opt = edit.toCode();
+    *fun = std::move(*opt);
+    delete opt;
+}
+}
+
 class Optimizer {
   public:
-    static void optimize(Code* fun) {}
+    static void optimize(Code* fun) {
+        for (int i = 0; i < 2; ++i) {
+            std::cout << " ====================== \n";
+            optimize_(fun);
+        }
+    }
 };
 
 } // rir