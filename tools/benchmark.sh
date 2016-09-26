9136f9e3e879d7758172568d403b7957d15fc1b8
oli <o@o1o.ch>
2016-09-26 13:59:45 +0200

    fix benchmark script

--- tools/benchmark.sh
+++ tools/benchmark.sh
@@ -1,17 +1,18 @@
 SCRIPTPATH=`cd $(dirname "$0") && pwd`
 PLAIN_R=~/src/freshr/R-3-2-branch/bin/R
 BASE=`cd $SCRIPTPATH/.. && pwd`
+TIMEOUT=120
 
 for i in `ls ${BASE}/benchmarks/shootout/*/*.r`; do
     T=`basename $i`;
     echo $T;
-    D=`dirname $i`;
-    R_JIT_ENABLE=3 /usr/bin/time -f'%E' -- ${SCRIPTPATH}/R -e "{setwd('$D'); source('$T'); execute()}" > /dev/null;
-    R_JIT_ENABLE=0 /usr/bin/time -f'%E' -- ${SCRIPTPATH}/R -e "{setwd('$D'); source('$T'); execute()}" > /dev/null;
-    R_JIT_ENABLE=3 /usr/bin/time -f'%E' -- $PLAIN_R -e "{setwd('$D'); source('$T'); execute()}" > /dev/null;
-    R_JIT_ENABLE=2 /usr/bin/time -f'%E' -- $PLAIN_R -e "{setwd('$D'); source('$T'); execute()}" > /dev/null;
-    R_JIT_ENABLE=0 /usr/bin/time -f'%E' -- $PLAIN_R -e "{setwd('$D'); source('$T'); execute()}" > /dev/null;
-    /usr/bin/time -f'%E' -- ${SCRIPTPATH}/R -e "{rir.enableJit(level=2, type='sticky');setwd('$D'); source('$T'); execute()}" > /dev/null;
-    /usr/bin/time -f'%E' -- ${SCRIPTPATH}/R -e "{rir.enableJit(level=3, type='sticky');setwd('$D'); source('$T'); execute()}" > /dev/null;
-    /usr/bin/time -f'%E' -- ${SCRIPTPATH}/R -e "{rir.enableJit(level=3, type='force');setwd('$D'); source('$T'); execute()}" > /dev/null;
+    D="`dirname $i`/../..";
+    R_JIT_ENABLE=3 timeout $TIMEOUT /usr/bin/time -f'%E' -- ${SCRIPTPATH}/R -e "{setwd('$D'); source('$i'); execute()}" > /dev/null;
+    R_JIT_ENABLE=0 timeout $TIMEOUT /usr/bin/time -f'%E' -- ${SCRIPTPATH}/R -e "{setwd('$D'); source('$i'); execute()}" > /dev/null;
+    R_JIT_ENABLE=3 timeout $TIMEOUT /usr/bin/time -f'%E' -- $PLAIN_R -e "{setwd('$D'); source('$i'); execute()}" > /dev/null;
+    R_JIT_ENABLE=2 timeout $TIMEOUT /usr/bin/time -f'%E' -- $PLAIN_R -e "{setwd('$D'); source('$i'); execute()}" > /dev/null;
+    R_JIT_ENABLE=0 timeout $TIMEOUT /usr/bin/time -f'%E' -- $PLAIN_R -e "{setwd('$D'); source('$i'); execute()}" > /dev/null;
+    timeout $TIMEOUT /usr/bin/time -f'%E' -- ${SCRIPTPATH}/R -e "{rir.enableJit(level=2, type='sticky');setwd('$D'); source('$i'); execute()}" > /dev/null;
+    timeout $TIMEOUT /usr/bin/time -f'%E' -- ${SCRIPTPATH}/R -e "{rir.enableJit(level=3, type='sticky');setwd('$D'); source('$i'); execute()}" > /dev/null;
+    timeout $TIMEOUT /usr/bin/time -f'%E' -- ${SCRIPTPATH}/R -e "{rir.enableJit(level=3, type='force');setwd('$D'); source('$i'); execute()}" > /dev/null;
 done 
