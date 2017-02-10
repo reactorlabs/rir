fbf7f85a68f1735cf4f9876c06c905b69fb13afd
o <o@o1o.ch>
2017-02-10 12:36:21 +0100

    revive travis integration

--- tools/ci/before_install-osx.sh
+++ tools/ci/before_install-osx.sh
@@ -4,11 +4,5 @@ echo
 echo Running before_install-osx.sh...
 echo
 
-echo Installing python3
-brew install python3 >> brew.log
-
-echo Installing doxygen
-brew install doxygen >> brew.log
-
 echo Installing ninja
 brew install ninja >> brew.log
