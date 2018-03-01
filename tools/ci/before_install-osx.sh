8b5c819ebd923c5b858322f83762f121eee48bb6
o <o@o1o.ch>
2018-03-01 18:01:48 +0100

    [travis] bump xcode version & valgrind

--- tools/ci/before_install-osx.sh
+++ tools/ci/before_install-osx.sh
@@ -4,10 +4,8 @@ echo
 echo Running before_install-osx.sh...
 echo
 
-echo Installing ninja
-brew install ninja
-
-echo Installing gcc/fortran
 brew install gcc
+brew link --overwrite gcc
 
 brew install xz
+#brew install valgrind
