067cc023afe8e9e0dc1b4aa8c2449cd62bf4e11a
Guido Chari <charig@gmail.com>
2018-08-06 11:03:07 +0200

    Make verbose type coherent. Change from macros definitions to c consts

--- rir/src/compiler/pir/pir.h
+++ rir/src/compiler/pir/pir.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+// clang-format off
 const uint8_t PRINT_ORIGINAL_MASK   = 0x1;  // bit 1: RIR Original version
 const uint8_t PRINT_RAW_PIR_MASK    = 0x2;  // bit 2: PIR Raw
 const uint8_t PRINT_OPT_PHASES_MASK = 0x4;  // bit 3: PIR Optimization phases
@@ -11,7 +12,7 @@ const uint8_t PRINT_CSSA_MASK       = 0x10; // bit 5: PIR Convert to CSSA
 const uint8_t PRINT_LIVENESS_MASK   = 0x20; // bit 6: PIR After liveness analysis phase
 const uint8_t PRINT_STACK_MASK      = 0x40; // bit 7: RIR After stack allocation phase
 const uint8_t PRINT_FINAL_RIR_MASK  = 0x80; // bit 8: RIR After passing through PIR
-
+// clang-format on
 
 #include "type.h"
 // Forward declaration of PIR types. Use for headers.
