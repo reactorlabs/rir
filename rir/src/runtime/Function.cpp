d6cae52bea38d3caea915f3b6b5dcf075f1c4221
Jan Ječmen <jan.jecmen@gmail.com>
2018-10-03 16:55:44 +0200

    tweak how we disassemble

--- rir/src/runtime/Function.cpp
+++ rir/src/runtime/Function.cpp
@@ -5,7 +5,7 @@ void Function::disassemble(std::ostream& out) {
     body()->disassemble(out);
     for (auto c : *this) {
         if (c != body()) {
-            out << "\n [Prom (index " << c->index << ")]\n";
+            out << "\n[Prom (index " << c->index << ")]\n";
             c->disassemble(out);
         }
     }
