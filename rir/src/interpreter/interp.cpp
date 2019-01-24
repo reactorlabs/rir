c4a0a4e81a709ee3d3ec38f2266a7213d10fea18
Jakobeha <jakobeha@gmail.com>
2019-01-24 15:23:31 -0500

    fixed some bugs

--- rir/src/interpreter/interp.cpp
+++ rir/src/interpreter/interp.cpp
@@ -2506,6 +2506,47 @@ SEXP evalRirCode(Code* c, Context* ctx, SEXP* env, const CallContext* callCtxt,
             NEXT();
         }
 
+        INSTRUCTION(subassign1_2_) {
+            SEXP idx2 = ostack_at(ctx, 0);
+            SEXP idx1 = ostack_at(ctx, 1);
+            SEXP vec = ostack_at(ctx, 2);
+            SEXP val = ostack_at(ctx, 3);
+
+            if (MAYBE_SHARED(vec)) {
+                vec = Rf_duplicate(vec);
+                ostack_set(ctx, 1, vec);
+            }
+
+            SEXP args = CONS_NR(
+                vec, CONS_NR(idx1, CONS_NR(idx2, CONS_NR(val, R_NilValue))));
+            SET_TAG(CDDDR(args), symbol::value);
+            PROTECT(args);
+
+            res = nullptr;
+            SEXP call = getSrcForCall(c, pc - 1, ctx);
+            SEXP selector = CAR(call) == symbol::SuperAssign
+                                ? symbol::SuperAssign2Bracket
+                                : symbol::Assign2Bracket;
+            RCNTXT assignContext;
+            Rf_begincontext(&assignContext, CTXT_RETURN, call, *env,
+                            ENCLOS(*env), args, selector);
+            if (isObject(vec)) {
+                res = dispatchApply(call, vec, args, selector, *env, ctx);
+            }
+            if (!res) {
+                res = do_subassign_dflt(call, selector, args, *env);
+                // We duplicated the vector above, and there is a stvar
+                // following
+                SET_NAMED(res, 0);
+            }
+            Rf_endcontext(&assignContext);
+            ostack_popn(ctx, 4);
+            UNPROTECT(1);
+
+            ostack_push(ctx, res);
+            NEXT();
+        }
+
         INSTRUCTION(subassign2_1_) {
             SEXP idx = ostack_at(ctx, 0);
             SEXP vec = ostack_at(ctx, 1);
@@ -2586,54 +2627,13 @@ SEXP evalRirCode(Code* c, Context* ctx, SEXP* env, const CallContext* callCtxt,
             }
 
             if (!res) {
-                res = do_subassign_dflt(call, selector, args, *env);
+                res = do_subassign2_dflt(call, selector, args, *env);
                 // We duplicated the vector above, and there is a stvar
                 // following
                 SET_NAMED(res, 0);
             }
             Rf_endcontext(&assignContext);
             ostack_popn(ctx, 3);
-            UNPROTECT(1);
-
-            ostack_push(ctx, res);
-            NEXT();
-        }
-
-        INSTRUCTION(subassign1_2_) {
-            SEXP idx2 = ostack_at(ctx, 0);
-            SEXP idx1 = ostack_at(ctx, 1);
-            SEXP vec = ostack_at(ctx, 2);
-            SEXP val = ostack_at(ctx, 3);
-
-            if (MAYBE_SHARED(vec)) {
-                vec = Rf_duplicate(vec);
-                ostack_set(ctx, 1, vec);
-            }
-
-            SEXP args = CONS_NR(
-                vec, CONS_NR(idx1, CONS_NR(idx2, CONS_NR(val, R_NilValue))));
-            SET_TAG(CDDR(args), symbol::value);
-            PROTECT(args);
-
-            res = nullptr;
-            SEXP call = getSrcForCall(c, pc - 1, ctx);
-            SEXP selector = CAR(call) == symbol::SuperAssign
-                                ? symbol::SuperAssign2Bracket
-                                : symbol::Assign2Bracket;
-            RCNTXT assignContext;
-            Rf_begincontext(&assignContext, CTXT_RETURN, call, *env,
-                            ENCLOS(*env), args, selector);
-            if (isObject(vec)) {
-                res = dispatchApply(call, vec, args, selector, *env, ctx);
-            }
-            if (!res) {
-                res = do_subassign_dflt(call, selector, args, *env);
-                // We duplicated the vector above, and there is a stvar
-                // following
-                SET_NAMED(res, 0);
-            }
-            Rf_endcontext(&assignContext);
-            ostack_popn(ctx, 4);
             UNPROTECT(1);
 
             ostack_push(ctx, res);
@@ -2722,7 +2722,7 @@ SEXP evalRirCode(Code* c, Context* ctx, SEXP* env, const CallContext* callCtxt,
 
             SEXP args = CONS_NR(
                 vec, CONS_NR(idx1, CONS_NR(idx2, CONS_NR(val, R_NilValue))));
-            SET_TAG(CDDR(args), symbol::value);
+            SET_TAG(CDDDR(args), symbol::value);
             PROTECT(args);
 
             res = nullptr;
