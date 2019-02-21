5917153543e8d0f8d72fa22cab677e8f024f5c0e
Guido Chari <charig@gmail.com>
2019-02-21 11:25:03 +0100

    Add moved files

---
+++ rir/src/interpreter/LazyEnvironment.h
@@ -0,0 +1,53 @@
+#ifndef LAZY_ENVIRONMENT_H
+#define LAZY_ENVIRONMENT_H
+
+#include "../ir/BC_inc.h"
+#include "RirDataWrapper.h"
+#include "interp_incl.h"
+
+#include <cassert>
+#include <cstdint>
+#include <functional>
+
+namespace rir {
+
+#define LAZY_ENVIRONMENT_MAGIC 0xe4210e47
+
+/**
+ * EnvironmentStub holds the information needed to create an
+ * environment lazily.
+ */
+struct LazyEnvironment
+    : public RirDataWrapper<LazyEnvironment, LAZY_ENVIRONMENT_MAGIC> {
+    LazyEnvironment() = delete;
+    LazyEnvironment(const LazyEnvironment&) = delete;
+    LazyEnvironment& operator=(const LazyEnvironment&) = delete;
+
+    // We need to store the arguments + the parent + *opcode for the names
+    LazyEnvironment(std::vector<SEXP>* arguments, SEXP parent, Opcode* opcode,
+                    InterpreterInstance* cmpCtx, R_bcstack_t* localsBase)
+        : RirDataWrapper(5), arguments_(arguments), parent_(parent),
+          names_(opcode), cmpCtx_(cmpCtx), localsBase_(localsBase){};
+    ~LazyEnvironment() { delete arguments_; }
+
+    const std::vector<SEXP>* arguments() { return arguments_; }
+    const SEXP parent() { return parent_; }
+    const Opcode* names() { return names_; }
+    InterpreterInstance* cmpCtx() { return cmpCtx_; }
+    R_bcstack_t* localsBase() { return localsBase_; }
+
+    SEXP create() {
+        return createEnvironment(arguments(), parent(), names(), cmpCtx(),
+                                 localsBase(), (SEXP)this);
+    }
+
+  private:
+    const std::vector<SEXP>* arguments_;
+    const SEXP parent_;
+    const Opcode* names_;
+    InterpreterInstance* cmpCtx_;
+    R_bcstack_t* localsBase_;
+};
+} // namespace rir
+
+#endif
---
+++ rir/src/interpreter/RirDataWrapper.h
@@ -0,0 +1,53 @@
+#ifndef RIR_DATA_WRAPPER_H
+#define RIR_DATA_WRAPPER_H
+
+/** RirDataWrappers are wrappers (usually over SEXP) of relevant
+ * RIR level information that must be shared to the gnu-r
+ * interpreter. For instance for the interpreter to lazily
+ * create stuff depending on RIR information
+ */
+
+#include "R/r.h"
+#include <cassert>
+#include <cstdint>
+
+namespace rir {
+
+#define RIR_DATA_WRAPPER_MAGIC 0xda7a0403
+
+struct data_header {
+    // magic number to differentiate RIR objects needs sync with gnur context.c
+    const uint32_t magic;
+
+    // magic number to differentiate different RIR data wrappers
+    const uint32_t wrapper;
+};
+
+template <typename BASE, uint32_t MAGIC>
+struct RirDataWrapper {
+    data_header info;
+
+    static BASE* check(void* s) {
+        BASE* b = (BASE*)s;
+        return b->info.wrapper == MAGIC ? b : nullptr;
+    }
+
+    static BASE* unpack(void* s) {
+        BASE* b = (BASE*)s;
+        assert(
+            b->info.wrapper == MAGIC &&
+            "Trying to unpack the wrong type of embedded RIR runtime object.");
+        return b;
+    }
+
+  protected:
+    explicit RirDataWrapper(uint32_t count)
+        : info{RIR_DATA_WRAPPER_MAGIC, MAGIC} {
+        uint8_t* start = (uint8_t*)this + sizeof(uint32_t) * 2;
+        memset(start, 0, count * sizeof(void*));
+    }
+};
+
+} // namespace rir
+
+#endif
