4b7319af50b120632fc09937ed06b0e44b0c470c
Jan Ječmen <jan.jecmen@gmail.com>
2019-03-11 19:07:21 +0100

    add regression test

---
+++ rir/tests/pir_regression8.R
@@ -0,0 +1,23 @@
+foo <- function() {
+	    size = 10L
+    sum = 0
+        y = 0
+        while (y < size) {
+		      x = 0
+	      while (x < size) {
+		              sum = 10
+	              x = x + 1
+		            }
+	            y = y + 1
+	          }
+	    sum
+}
+
+ex <- function() foo()
+
+ex()
+ex()
+ex()
+ex()
+ex()
+
