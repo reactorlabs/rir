370b970f860cebce65aa18c941fd2160d49913ab
Jakobeha <32147996+Jakobeha@users.noreply.github.com>
2019-03-13 15:33:45 -0400

    make all closure prints use options

--- rir/src/compiler/debugging/stream_logger.cpp
+++ rir/src/compiler/debugging/stream_logger.cpp
@@ -68,19 +68,7 @@ void LogStream::pirOptimizationsFinished(ClosureVersion* closure) {
         std::regex_match(name.begin(), name.end(), options.functionFilter)) {
         preparePrint();
         section("PIR Version After Optimizations");
-        switch (options.style) {
-        case DebugStyle::Standard:
-            closure->print(out, tty());
-            break;
-        case DebugStyle::GraphViz:
-            closure->printGraph(out, tty());
-            break;
-        case DebugStyle::GraphVizBB:
-            closure->printBBGraph(out, tty());
-            break;
-        default:
-            assert(false);
-        }
+        closure->print(out, tty());
         out << "\n";
     }
 }
