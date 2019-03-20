4fb711c4a3d1a375f7bbfbf5339065575933644f
Guido Chari <charig@gmail.com>
2019-03-20 11:37:31 +0100

    Rollback dt

--- rir/src/runtime/DispatchTable.h
+++ rir/src/runtime/DispatchTable.h
@@ -80,7 +80,7 @@ struct DispatchTable
         SLOWASSERT(contains(fun->signature().assumptions));
     }
 
-    static DispatchTable* create(size_t capacity = 12) {
+    static DispatchTable* create(size_t capacity = 10) {
         size_t size =
             sizeof(DispatchTable) + (capacity * sizeof(DispatchTableEntry));
         SEXP s = Rf_allocVector(EXTERNALSXP, size);
