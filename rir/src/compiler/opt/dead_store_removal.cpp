1cb4372622e10e09fd5e654b6b8b3c8222ea6035
Guido Chari <charig@gmail.com>
2019-03-22 15:03:46 +0100

    merge

---
+++ rir/src/compiler/opt/dead_store_removal.cpp
@@ -0,0 +1,35 @@
+#include "../analysis/dead_store.h"
+#include "pass_definitions.h"
+
+namespace rir {
+namespace pir {
+
+void DeadStoreRemoval::apply(RirCompiler&, ClosureVersion* function,
+                             LogStream& log) const {
+    bool noStores = Visitor::check(
+        function->entry, [&](Instruction* i) { return !StVar::Cast(i); });
+    if (noStores)
+        return;
+
+    {
+        CFG cfg(function);
+        DeadStoreAnalysis analysis(function, cfg, log);
+
+        Visitor::run(function->entry, [&](BB* bb) {
+            auto ip = bb->begin();
+            while (ip != bb->end()) {
+                auto next = ip + 1;
+                if (auto st = StVar::Cast(*ip)) {
+                    if (analysis.isDead(st)) {
+                        next = bb->remove(ip);
+                        continue;
+                    }
+                }
+                ip = next;
+            }
+        });
+    }
+}
+
+} // namespace pir
+} // namespace rir
