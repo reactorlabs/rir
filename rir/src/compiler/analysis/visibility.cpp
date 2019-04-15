15f899d88840c79f7482e21108c64ea311c9133a
Jakobeha <jakobeha@gmail.com>
2019-04-15 10:48:35 -0400

    finish rebase

--- rir/src/compiler/analysis/visibility.cpp
+++ rir/src/compiler/analysis/visibility.cpp
@@ -7,46 +7,19 @@ namespace pir {
 AbstractResult VisibilityAnalysis::apply(LastVisibilityUpdate& vis,
                                          Instruction* i) const {
     AbstractResult res;
-    auto changesVisibility = [&]() {
+    if (i->alwaysOverridesVisibility()) {
+        // Always changes visibility
         if (vis.observable.size() == 1 && *vis.observable.begin() == i)
             return;
         vis.observable.clear();
         vis.observable.insert(i);
         res.update();
-    };
-    auto maybeChangesVisibility = [&]() {
+    } else if (i->effects.contains(Effect::Visibility)) {
+        // Maybe changes visibility
         if (!vis.observable.count(i)) {
             vis.observable.insert(i);
             res.update();
         }
-    };
-
-    switch (i->tag) {
-    case Tag::Invisible:
-    case Tag::Visible:
-        changesVisibility();
-        break;
-    case Tag::CallBuiltin:
-    case Tag::CallSafeBuiltin:
-        int builtinId;
-        if (auto c = CallBuiltin::Cast(i)) {
-            builtinId = c->builtinId;
-        } else {
-            builtinId = CallSafeBuiltin::Cast(i)->builtinId;
-        }
-
-        if (builtinUpdatesVisibility(builtinId))
-            changesVisibility();
-        break;
-
-    default:
-        // This instruction might change visibility, thus it's visibility effect
-        // might be observable. But it does not clear previous visibility
-        // instructions, because it might also preserve the previous visibility
-        // setting.
-        if (i->effects.contains(Effect::Visibility))
-            maybeChangesVisibility();
-        break;
     }
     return res;
 };
