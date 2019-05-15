96eb52282d349de247de7df527deaa963599bdc3
Jakobeha <jakobeha@gmail.com>
2019-05-15 02:17:20 +0200

    ?

--- rir/src/compiler/pir/type.h
+++ rir/src/compiler/pir/type.h
@@ -394,7 +394,7 @@ struct PirType {
                 t.setNotScalar();
             return t;
         } else if (t_.r.contains(RType::prom)) {
-            return PirType::val();
+            return val();
         } else {
             return forced().notObject().orNotScalar() | RType::vec;
         }
