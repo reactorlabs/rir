396fee936004fa2de8ebf77856d85e638787ce67
oli <o@o1o.ch>
2019-05-28 12:40:07 +0200

    update pinned containers

--- container/cleanup.rb
+++ container/cleanup.rb
@@ -14,7 +14,7 @@ REPOS = {
       `git rev-parse HEAD~1`.chomp,                 # prev version
       'master',
       'c13be9ca843726e3eb3e57e6e6f2a602ae7481eb',   # referenced in paper
-      '3ae6a44d3a3ad5d643991b24e5c29654b053a854',
+      'e4cee663a3fdd0455592d4835b08716234882024',
     ]},
   '12325205': {# rir experiments
     repos: [
@@ -23,7 +23,7 @@ REPOS = {
     ],
     keep: [
       'c13be9ca843726e3eb3e57e6e6f2a602ae7481eb-210646f2a98099ed48d928b1f5e66e551aa0d92b',
-      '3ae6a44d3a3ad5d643991b24e5c29654b053a854-b2a9331e8ffb822828595835f90da4c366d014bd',
+      'e4cee663a3fdd0455592d4835b08716234882024-b2a9331e8ffb822828595835f90da4c366d014bd',
     ]},
 }
 
