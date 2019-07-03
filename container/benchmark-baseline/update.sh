d061f938b7c0c5a6507ce1a8bc31d170867e45e2
oli <o@o1o.ch>
2019-07-03 09:42:28 +0200

    use gitlab ci to trigger benchmark runs

--- container/benchmark-baseline/update.sh
+++ container/benchmark-baseline/update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t registry.gitlab.com/rirvm/rir_mirror/benchmark-baseline .
+docker build --no-cache -t registry.gitlab.com/rirvm/rir_mirror/benchmark-baseline .
 docker push registry.gitlab.com/rirvm/rir_mirror/benchmark-baseline
