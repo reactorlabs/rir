2b31ce48a7b555261757dd2fe05feef4b668dee4
oli <o@o1o.ch>
2019-10-03 11:32:49 +0000

    remove less checkpoints

--- rir/src/compiler/opt/assumptions.cpp
+++ rir/src/compiler/opt/assumptions.cpp
@@ -88,18 +88,20 @@ void OptimizeAssumptions::apply(RirCompiler&, ClosureVersion* function,
             // the previous checkpoint is still available, and there is also a
             // next checkpoint available we might as well remove this one.
             if (auto cp = Checkpoint::Cast(instr)) {
-                if (auto cp0 = checkpoint.at(instr)) {
-                    while (replaced.count(cp0))
-                        cp0 = replaced.at(cp0);
-                    replaced[cp] = cp0;
+                if (!cp->nextBB()->isEmpty() &&
+                    checkpoint.next(*cp->nextBB()->begin()))
+                    if (auto cp0 = checkpoint.at(instr)) {
+                        while (replaced.count(cp0))
+                            cp0 = replaced.at(cp0);
+                        replaced[cp] = cp0;
 
-                    assert(bb->last() == instr);
-                    cp->replaceUsesWith(cp0);
-                    bb->remove(ip);
-                    delete bb->next1;
-                    bb->next1 = nullptr;
-                    return;
-                }
+                        assert(bb->last() == instr);
+                        cp->replaceUsesWith(cp0);
+                        bb->remove(ip);
+                        delete bb->next1;
+                        bb->next1 = nullptr;
+                        return;
+                    }
             }
 
             if (auto assume = Assume::Cast(instr)) {
