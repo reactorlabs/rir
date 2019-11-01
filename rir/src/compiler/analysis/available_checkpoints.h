a9bdb424d1bb4da648a3ec2db1ccc4b0bb2eb624
Guido Chari <charig@gmail.com>
2019-11-01 11:21:36 +0100

    Instead of returning the earliest avaialable checkpoint (which is not possible using this lattice), we return the latest

--- rir/src/compiler/analysis/available_checkpoints.h
+++ rir/src/compiler/analysis/available_checkpoints.h
@@ -11,16 +11,13 @@ namespace pir {
 struct AvailableCheckpointsApply {
     static AbstractResult apply(AbstractUnique<Checkpoint>& state,
                                 Instruction* i) {
-        if (state.get()) {
-            if (i->isDeoptBarrier()) {
-                state.clear();
-                return AbstractResult::Updated;
-            }
-        } else {
-            if (auto cp = Checkpoint::Cast(i)) {
-                state.set(cp);
-                return AbstractResult::Updated;
-            }
+        if (state.get() && i->isDeoptBarrier()) {
+            state.clear();
+            return AbstractResult::Updated;
+        }
+        if (auto cp = Checkpoint::Cast(i)) {
+            state.set(cp);
+            return AbstractResult::Updated;
         }
         return AbstractResult::None;
     }
