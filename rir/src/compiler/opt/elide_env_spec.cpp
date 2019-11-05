e6a733acd9409dfd2affe014e5272831136026f6
Guido Chari <charig@gmail.com>
2019-11-05 11:44:59 +0100

    If we materialize on deopt branches we can elide environments used by any instruction if it is only in a deopt branch

--- rir/src/compiler/opt/elide_env_spec.cpp
+++ rir/src/compiler/opt/elide_env_spec.cpp
@@ -123,8 +123,7 @@ void ElideEnvSpec::apply(RirCompiler&, ClosureVersion* function,
     // since it can only be accessed reflectively.
     static std::unordered_set<Tag> allowed{
         Tag::Force, Tag::FrameState, Tag::PushContext, Tag::LdVar,
-        Tag::StVar, Tag::StVarSuper, Tag::MkArg};
-
+        Tag::StVar, Tag::StVarSuper};
     Visitor::run(function->entry, [&](Instruction* i) {
         i->eachArg([&](Value* val) {
             if (auto m = MkEnv::Cast(val)) {
@@ -140,20 +139,18 @@ void ElideEnvSpec::apply(RirCompiler&, ClosureVersion* function,
                                 bannedEnvs.insert(m);
                         return;
                     }
-                    // It is safe to elide environments used by mkArg only from
-                    // deopt branches
-                    if (MkArg::Cast(i) && !MkArg::Cast(i)->bb()->isDeopt())
+                    // It is safe to elide environments used in deopt branches
+                    // if we materialize them just before usage
+                    if (!i->bb()->isDeopt() && !allowed.count(i->tag)){
                         bannedEnvs.insert(m);
-
-                    if (!allowed.count(i->tag))
-                        bannedEnvs.insert(m);
+                    }
                 }
             }
         });
     });
 
     std::unordered_map<Instruction*, std::pair<Checkpoint*, MkEnv*>> checks;
-    std::unordered_map<MkEnv*, SmallSet<MkArg*>> needsMaterialization;
+    std::unordered_map<MkEnv*, SmallSet<Instruction*>> needsMaterialization;
     Visitor::run(function->entry, [&](Instruction* i) {
         if (i->hasEnv()) {
             if (FrameState::Cast(i) || StVar::Cast(i) || LdVar::Cast(i) ||
@@ -162,8 +159,8 @@ void ElideEnvSpec::apply(RirCompiler&, ClosureVersion* function,
             if (auto mk = MkEnv::Cast(i->env())) {
                 if (mk->stub || bannedEnvs.count(mk))
                     return;
-                if (auto mkArg = MkArg::Cast(i)) {
-                    needsMaterialization[mk].insert(mkArg);
+                if (i->bb()->isDeopt()) {
+                    needsMaterialization[mk].insert(i);
                 } else {
                     // We can only stub an environment if all uses have a
                     // checkpoint available after every use.
