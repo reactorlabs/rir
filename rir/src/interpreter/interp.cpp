f7b39c5b32f65107a674822113ca9567f7e88796
jakob <jakobeha@gmail.com>
2020-03-13 12:16:34 +0100

    return NativeType::test SEXPs from colonInputEffects

--- rir/src/interpreter/interp.cpp
+++ rir/src/interpreter/interp.cpp
@@ -1641,7 +1641,7 @@ static bool doubleCanBeCastedToInteger(double n) {
     return n >= INT_MIN && n <= INT_MAX && (int)n == n;
 }
 
-bool colonInputEffects(SEXP lhs, SEXP rhs, unsigned srcIdx) {
+SEXP colonInputEffects(SEXP lhs, SEXP rhs, unsigned srcIdx) {
     auto getSrc = [&]() { return src_pool_at(globalContext(), srcIdx); };
 
     // 1. decide fastcase
@@ -1674,7 +1674,7 @@ bool colonInputEffects(SEXP lhs, SEXP rhs, unsigned srcIdx) {
                            (int)rhsLen);
     }
 
-    return fastcase;
+    return fastcase ? R_TrueValue : R_FalseValue;
 }
 
 SEXP colonCastLhs(SEXP lhs) {
@@ -3209,8 +3209,8 @@ SEXP evalRirCode(Code* c, InterpreterInstance* ctx, SEXP env,
             SEXP lhs = ostack_at(ctx, 1);
             SEXP rhs = ostack_at(ctx, 0);
 
-            bool fastcase = colonInputEffects(lhs, rhs, 0);
-            ostack_push(ctx, fastcase ? R_TrueValue : R_FalseValue);
+            SEXP fastcaseSexp = colonInputEffects(lhs, rhs, 0);
+            ostack_push(ctx, fastcaseSexp);
 
             NEXT();
         }
