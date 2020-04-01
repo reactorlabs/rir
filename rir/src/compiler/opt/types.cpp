260c655460fcfaa6d1a5a5651a37727f33394d25
jakob <jakobeha@gmail.com>
2020-04-01 17:37:50 +0200

    fixed incorrect non-NA inference on builtins

--- rir/src/compiler/opt/types.cpp
+++ rir/src/compiler/opt/types.cpp
@@ -55,16 +55,12 @@ void TypeInference::apply(RirCompiler&, ClosureVersion* function,
                         if (getType(c->callArg(0).val()).isScalar() &&
                             getType(c->callArg(1).val()).isScalar())
                             inferred.setScalar();
-                        if (!getType(c->callArg(0).val()).maybeNAOrNaN() &&
-                            !getType(c->callArg(1).val()).maybeNAOrNaN())
-                            inferred.setNotNAOrNaN();
                         break;
                     }
 
                     if ("length" == name) {
-                        inferred = (PirType() | RType::integer | RType::real)
-                                       .scalar()
-                                       .notNAOrNaN();
+                        inferred =
+                            (PirType() | RType::integer | RType::real).scalar();
                         break;
                     }
 
@@ -113,8 +109,6 @@ void TypeInference::apply(RirCompiler&, ClosureVersion* function,
                             inferred = PirType(RType::integer);
                             if (getType(c->callArg(0).val()).isScalar())
                                 inferred.setScalar();
-                            if (!getType(c->callArg(0).val()).maybeNAOrNaN())
-                                inferred.setNotNAOrNaN();
                         } else {
                             inferred = i->inferType(getType);
                         }
@@ -133,8 +127,6 @@ void TypeInference::apply(RirCompiler&, ClosureVersion* function,
                             inferred = PirType(RType::logical);
                             if (getType(c->callArg(0).val()).isScalar())
                                 inferred.setScalar();
-                            if (!getType(c->callArg(0).val()).maybeNAOrNaN())
-                                inferred.setNotNAOrNaN();
                         } else {
                             inferred = i->inferType(getType);
                         }
