b79b08dc307872d3352a5f9b76f6b77fa30ebcf7
Sebastian Krynski <skrynski@gmail.com>
2020-04-16 17:12:27 +0000

    reverting liveness.cpp again

--- rir/src/compiler/analysis/liveness.cpp
+++ rir/src/compiler/analysis/liveness.cpp
@@ -23,144 +23,141 @@ LivenessIntervals::LivenessIntervals(Code* code, unsigned bbsSize) {
             todo.insert(bb);
     });
 
+restart:
     while (!todo.empty()) {
+        BB* bb = *todo.begin();
+        todo.erase(todo.begin());
 
-        while (!todo.empty()) {
-            BB* bb = *todo.begin();
-            todo.erase(todo.begin());
+        // keep track of currently live variables
+        // ordered sets so we can use std::includes
+        std::set<Value*> accumulated;
+        std::unordered_map<BB*, std::set<Value*>> accumulatedPhiInput;
 
-            // keep track of currently live variables
-            // ordered sets so we can use std::includes
-            std::set<Value*> accumulated;
-            std::unordered_map<BB*, std::set<Value*>> accumulatedPhiInput;
-
-            // Mark all (backwards) incoming live variables
-            for (const auto& v : liveAtEnd[bb]) {
-                assert(count(v));
-                auto& liveRange = intervals[v][bb->id];
-                if (!liveRange.live || liveRange.end < bb->size()) {
-                    liveRange.live = true;
-                    liveRange.end = bb->size();
-                    accumulated.insert(v);
-                }
-            }
-
-            // Run BB in reverse
-            size_t pos = bb->size();
-            if (!bb->isEmpty()) {
-                auto ip = bb->end();
-                do {
-                    --ip;
-                    --pos;
-                    Instruction* i = *ip;
-
-                    auto markIfNotSeen = [&](Value* v) {
-                        if (!count(v)) {
-                            // First time we see this variable, need to allocate
-                            // vector of all liveranges
-                            intervals[v].resize(bbsSize);
-                            assert(!intervals[v][bb->id].live);
-                        }
-                        auto& liveRange = intervals[v][bb->id];
-                        if (!liveRange.live) {
-                            liveRange.live = true;
-                            liveRange.end = pos;
-                            return true;
-                        }
-                        return false;
-                    };
-
-                    // First set all arguments to be live
-                    if (auto phi = Phi::Cast(i)) {
-                        phi->eachArg([&](BB* in, Value* v) {
-                            if (markIfNotSeen(v))
-                                accumulatedPhiInput[in].insert(v);
-                        });
-                    } else {
-                        i->eachArg([&](Value* v) {
-                            if (markIfNotSeen(v)) {
-                                accumulated.insert(v);
-                            }
-                        });
-                    }
-
-                    // Mark the end of the current instructions liveness
-                    if (accumulated.count(i)) {
-                        assert(count(i));
-                        auto& liveRange = intervals[i][bb->id];
-                        assert(liveRange.live);
-                        liveRange.begin = pos;
-                        accumulated.erase(accumulated.find(i));
-                    }
-
-                    if (accumulated.size() > maxLive)
-                        maxLive = accumulated.size();
-
-                } while (ip != bb->begin());
-            }
-            assert(pos == 0);
-
-            // Mark everything that is live at the beginning of the BB.
-            // Note that we need a separate `liveAtEntry` flag; begin = 0 cannot
-            // distinguish between liveness before or after the first
-            // instruction.
-            auto markLiveEntry = [&](Value* v) {
-                assert(count(v));
-                auto& liveRange = intervals[v][bb->id];
-                assert(liveRange.live);
-                liveRange.liveAtEntry = true;
-                liveRange.begin = 0;
-            };
-            for (const auto& v : accumulated)
-                markLiveEntry(v);
-            for (const auto& pi : accumulatedPhiInput)
-                for (const auto& v : pi.second)
-                    markLiveEntry(v);
-
-            // Merge everything that is live at the beginning of the BB into the
-            // incoming vars of all predecessors
-            //
-            // Phi inputs should only be merged to BB that are successors of the
-            // input BBs
-            auto merge = [&](BB* bb, const std::set<Value*>& live) {
-                auto& liveOut = liveAtEnd[bb];
-                if (!std::includes(liveOut.begin(), liveOut.end(), live.begin(),
-                                   live.end())) {
-                    liveOut.insert(live.begin(), live.end());
-                    todo.insert(bb);
-                }
-            };
-            auto mergePhiInp = [&](BB* bb) {
-                for (const auto& in : accumulatedPhiInput) {
-                    auto& inBB = in.first;
-                    auto& inLive = in.second;
-                    if (bb == inBB) {
-                        merge(bb, inLive);
-                    }
-                }
-            };
-            for (const auto& pre : bb->predecessors()) {
-                if (!liveAtEnd.count(pre)) {
-                    liveAtEnd[pre] = accumulated;
-                    mergePhiInp(pre);
-                    todo.insert(pre);
-                } else {
-                    merge(pre, accumulated);
-                    mergePhiInp(pre);
-                }
+        // Mark all (backwards) incoming live variables
+        for (const auto& v : liveAtEnd[bb]) {
+            assert(count(v));
+            auto& liveRange = intervals[v][bb->id];
+            if (!liveRange.live || liveRange.end < bb->size()) {
+                liveRange.live = true;
+                liveRange.end = bb->size();
+                accumulated.insert(v);
             }
         }
 
-        // enqueue nodes that are alive and have a non-live successor
-        // to be processed on the next iteration
-        for (auto p : liveAtEnd) {
-            auto bb = p.first;
-            for (auto n : bb->successors()) {
-                if (!liveAtEnd.count(n))
-                    todo.insert(n);
+        // Run BB in reverse
+        size_t pos = bb->size();
+        if (!bb->isEmpty()) {
+            auto ip = bb->end();
+            do {
+                --ip;
+                --pos;
+                Instruction* i = *ip;
+
+                auto markIfNotSeen = [&](Value* v) {
+                    if (!count(v)) {
+                        // First time we see this variable, need to allocate
+                        // vector of all liveranges
+                        intervals[v].resize(bbsSize);
+                        assert(!intervals[v][bb->id].live);
+                    }
+                    auto& liveRange = intervals[v][bb->id];
+                    if (!liveRange.live) {
+                        liveRange.live = true;
+                        liveRange.end = pos;
+                        return true;
+                    }
+                    return false;
+                };
+
+                // First set all arguments to be live
+                if (auto phi = Phi::Cast(i)) {
+                    phi->eachArg([&](BB* in, Value* v) {
+                        if (markIfNotSeen(v))
+                            accumulatedPhiInput[in].insert(v);
+                    });
+                } else {
+                    i->eachArg([&](Value* v) {
+                        if (markIfNotSeen(v)) {
+                            accumulated.insert(v);
+                        }
+                    });
+                }
+
+                // Mark the end of the current instructions liveness
+                if (accumulated.count(i)) {
+                    assert(count(i));
+                    auto& liveRange = intervals[i][bb->id];
+                    assert(liveRange.live);
+                    liveRange.begin = pos;
+                    accumulated.erase(accumulated.find(i));
+                }
+
+                if (accumulated.size() > maxLive)
+                    maxLive = accumulated.size();
+
+            } while (ip != bb->begin());
+        }
+        assert(pos == 0);
+
+        // Mark everything that is live at the beginning of the BB.
+        // Note that we need a separate `liveAtEntry` flag; begin = 0 cannot
+        // distinguish between liveness before or after the first instruction.
+        auto markLiveEntry = [&](Value* v) {
+            assert(count(v));
+            auto& liveRange = intervals[v][bb->id];
+            assert(liveRange.live);
+            liveRange.liveAtEntry = true;
+            liveRange.begin = 0;
+        };
+        for (const auto& v : accumulated)
+            markLiveEntry(v);
+        for (const auto& pi : accumulatedPhiInput)
+            for (const auto& v : pi.second)
+                markLiveEntry(v);
+
+        // Merge everything that is live at the beginning of the BB into the
+        // incoming vars of all predecessors
+        //
+        // Phi inputs should only be merged to BB that are successors of the
+        // input BBs
+        auto merge = [&](BB* bb, const std::set<Value*>& live) {
+            auto& liveOut = liveAtEnd[bb];
+            if (!std::includes(liveOut.begin(), liveOut.end(), live.begin(),
+                               live.end())) {
+                liveOut.insert(live.begin(), live.end());
+                todo.insert(bb);
+            }
+        };
+        auto mergePhiInp = [&](BB* bb) {
+            for (const auto& in : accumulatedPhiInput) {
+                auto& inBB = in.first;
+                auto& inLive = in.second;
+                if (bb == inBB) {
+                    merge(bb, inLive);
+                }
+            }
+        };
+        for (const auto& pre : bb->predecessors()) {
+            if (!liveAtEnd.count(pre)) {
+                liveAtEnd[pre] = accumulated;
+                mergePhiInp(pre);
+                todo.insert(pre);
+            } else {
+                merge(pre, accumulated);
+                mergePhiInp(pre);
             }
         }
     }
+
+    Visitor::run(code->entry, [&](BB* bb) {
+        for (auto n : bb->successors()) {
+            if (!liveAtEnd.count(n))
+                todo.insert(n);
+        }
+    });
+
+    if (!todo.empty())
+        goto restart;
 
 #ifdef DEBUG_LIVENESS
     for (const auto& kv : intervals) {
