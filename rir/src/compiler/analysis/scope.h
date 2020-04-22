f7b693e9fef1c3227a8f5780b83aca0bfa788361
oli <o@o1o.ch>
2020-04-22 20:59:14 +0000

    reduce size of interprocedurally scope analyzed functions

--- rir/src/compiler/analysis/scope.h
+++ rir/src/compiler/analysis/scope.h
@@ -88,7 +88,7 @@ class ScopeAnalysis
     const std::vector<Value*> args;
 
     static constexpr size_t MAX_DEPTH = 2;
-    static constexpr size_t MAX_SIZE = 1000;
+    static constexpr size_t MAX_SIZE = 200;
     static constexpr size_t MAX_RESULTS = 1000;
     size_t depth;
     Value* staticClosureEnv = Env::notClosed();
