917a6a6d6af3ef46664908f90655c6cc2fa8973c
Jan Ječmen <jan.jecmen@gmail.com>
2020-09-23 15:32:28 +0000

    typo

--- rir/src/api.h
+++ rir/src/api.h
@@ -15,7 +15,7 @@ REXPORT SEXP rir_invocation_count(SEXP what);
 REXPORT SEXP rir_eval(SEXP exp, SEXP env);
 REXPORT SEXP pir_compile(SEXP closure, SEXP name, SEXP debugFlags,
                          SEXP debugStyle);
-REXPORT SEXP rir_compile(SEXP what, SEXP env);
+REXPORT SEXP rirCompile(SEXP what, SEXP env);
 REXPORT SEXP pir_tests();
 REXPORT SEXP pir_check(SEXP f, SEXP check, SEXP env);
 REXPORT SEXP pir_setDebugFlags(SEXP debugFlags);
