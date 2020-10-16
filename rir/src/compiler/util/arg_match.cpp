62a027f15b22a88de0eb1f11a8ab1f0b30a6dce4
Jan Ječmen <jan.jecmen@gmail.com>
2020-10-16 13:02:37 +0000

    don't build unused args list

--- rir/src/compiler/util/arg_match.cpp
+++ rir/src/compiler/util/arg_match.cpp
@@ -1,5 +1,6 @@
 #include "arg_match.h"
 #include "../pir/pir_impl.h"
+#include "R/Protect.h"
 #include "utils/Pool.h"
 
 namespace rir {
@@ -28,7 +29,8 @@ bool ArgumentMatcher::reorder(Builder& insert, SEXP formals,
     }
 
     // The following code is mostly a copy of Rf_machArgs from main/match.c,
-    // where errors have been replaced by 'return false'
+    // where errors have been replaced by 'return false'. Also our Protect
+    // wrapper is used and the unused argument list is not built.
     Rboolean seendots;
     size_t i, arg_i = 0;
     SEXP f, a, b, dots, actuals;
@@ -55,163 +57,144 @@ bool ArgumentMatcher::reorder(Builder& insert, SEXP formals,
     for (b = supplied; b != R_NilValue; b = CDR(b))
         SET_ARGUSED(b, 0);
 
-    PROTECT(actuals);
+    {
+        Protect p(actuals);
 
-    /* First pass: exact matches by tag */
-    /* Grab matched arguments and check */
-    /* for multiple exact matches. */
+        /* First pass: exact matches by tag */
+        /* Grab matched arguments and check */
+        /* for multiple exact matches. */
 
-    f = formals;
-    a = actuals;
-    arg_i = 0;
-    while (f != R_NilValue) {
-        SEXP ftag = TAG(f);
-        const char* ftag_name = CHAR(PRINTNAME(ftag));
-        if (ftag != R_DotsSymbol && ftag != R_NilValue) {
-            for (b = supplied, i = 1; b != R_NilValue; b = CDR(b), i++) {
-                SEXP btag = TAG(b);
-                if (btag != R_NilValue) {
-                    const char* btag_name = CHAR(PRINTNAME(btag));
-                    if (streql(ftag_name, btag_name)) {
-                        if (fargused[arg_i] == 2) {
-                            UNPROTECT(1);
-                            return false;
-                        }
-                        if (ARGUSED(b) == 2) {
-                            UNPROTECT(1);
-                            return false;
-                        }
-                        SETCAR(a, CAR(b));
-                        if (CAR(b) != R_MissingArg)
-                            SET_MISSING(a, 0);
-                        SET_ARGUSED(b, 2);
-                        fargused[arg_i] = 2;
-                    }
-                }
-            }
-        }
-        f = CDR(f);
-        a = CDR(a);
-        arg_i++;
-    }
-
-    /* Second pass: partial matches based on tags */
-    /* An exact match is required after first ... */
-    /* The location of the first ... is saved in "dots" */
-
-    dots = R_NilValue;
-    seendots = FALSE;
-    f = formals;
-    a = actuals;
-    arg_i = 0;
-    while (f != R_NilValue) {
-        if (fargused[arg_i] == 0) {
-            if (TAG(f) == R_DotsSymbol && !seendots) {
-                /* Record where ... value goes */
-                dots = a;
-                seendots = TRUE;
-            } else {
+        f = formals;
+        a = actuals;
+        arg_i = 0;
+        while (f != R_NilValue) {
+            SEXP ftag = TAG(f);
+            const char* ftag_name = CHAR(PRINTNAME(ftag));
+            if (ftag != R_DotsSymbol && ftag != R_NilValue) {
                 for (b = supplied, i = 1; b != R_NilValue; b = CDR(b), i++) {
-                    if (ARGUSED(b) != 2 && TAG(b) != R_NilValue &&
-                        pmatch(TAG(f), TAG(b), seendots)) {
-                        UNPROTECT(1);
-                        return false;
+                    SEXP btag = TAG(b);
+                    if (btag != R_NilValue) {
+                        const char* btag_name = CHAR(PRINTNAME(btag));
+                        if (streql(ftag_name, btag_name)) {
+                            if (fargused[arg_i] == 2) {
+                                return false;
+                            }
+                            if (ARGUSED(b) == 2) {
+                                return false;
+                            }
+                            SETCAR(a, CAR(b));
+                            if (CAR(b) != R_MissingArg)
+                                SET_MISSING(a, 0);
+                            SET_ARGUSED(b, 2);
+                            fargused[arg_i] = 2;
+                        }
                     }
                 }
             }
-        }
-        f = CDR(f);
-        a = CDR(a);
-        arg_i++;
-    }
-
-    /* Third pass: matches based on order */
-    /* All args specified in tag=value form */
-    /* have now been matched.  If we find ... */
-    /* we gobble up all the remaining args. */
-    /* Otherwise we bind untagged values in */
-    /* order to any unmatched formals. */
-
-    f = formals;
-    a = actuals;
-    b = supplied;
-    seendots = FALSE;
-
-    while (f != R_NilValue && b != R_NilValue && !seendots) {
-        if (TAG(f) == R_DotsSymbol) {
-            /* Skip ... matching until all tags done */
-            seendots = TRUE;
             f = CDR(f);
             a = CDR(a);
-        } else if (CAR(a) != R_MissingArg) {
-            /* Already matched by tag */
-            /* skip to next formal */
-            f = CDR(f);
-            a = CDR(a);
-        } else if (ARGUSED(b) || TAG(b) != R_NilValue) {
-            /* This value used or tagged , skip to next value */
-            /* The second test above is needed because we */
-            /* shouldn't consider tagged values for positional */
-            /* matches. */
-            /* The formal being considered remains the same */
-            b = CDR(b);
-        } else {
-            /* We have a positional match */
-            SETCAR(a, CAR(b));
-            if (CAR(b) != R_MissingArg)
-                SET_MISSING(a, 0);
-            SET_ARGUSED(b, 1);
-            b = CDR(b);
-            f = CDR(f);
-            a = CDR(a);
+            arg_i++;
         }
-    }
 
-    size_t protectedVars = 1;
-    if (dots != R_NilValue) {
-        /* Gobble up all unused actuals */
-        SET_MISSING(dots, 0);
-        i = 0;
-        for (a = supplied; a != R_NilValue; a = CDR(a))
-            if (!ARGUSED(a))
-                i++;
+        /* Second pass: partial matches based on tags */
+        /* An exact match is required after first ... */
+        /* The location of the first ... is saved in "dots" */
 
-        if (i) {
-            a = allocList(i);
-            SET_TYPEOF(a, DOTSXP);
-            f = a;
-            for (b = supplied; b != R_NilValue; b = CDR(b))
-                if (!ARGUSED(b)) {
-                    SETCAR(f, CAR(b));
-                    SET_TAG(f, TAG(b));
-                    f = CDR(f);
-                }
-            SETCAR(dots, a);
-        }
-    } else {
-        /* Check that all arguments are used */
-        // cppcheck-suppress unreadVariable
-        SEXP unused = R_NilValue, last = R_NilValue;
-        for (b = supplied; b != R_NilValue; b = CDR(b))
-            if (!ARGUSED(b)) {
-                if (last == R_NilValue) {
-                    protectedVars++;
-                    PROTECT(unused = CONS_NR(CAR(b), R_NilValue));
-                    SET_TAG(unused, TAG(b));
-                    last = unused;
+        dots = R_NilValue;
+        seendots = FALSE;
+        f = formals;
+        a = actuals;
+        arg_i = 0;
+        while (f != R_NilValue) {
+            if (fargused[arg_i] == 0) {
+                if (TAG(f) == R_DotsSymbol && !seendots) {
+                    /* Record where ... value goes */
+                    dots = a;
+                    seendots = TRUE;
                 } else {
-                    SETCDR(last, CONS_NR(CAR(b), R_NilValue));
-                    last = CDR(last);
-                    SET_TAG(last, TAG(b));
+                    for (b = supplied, i = 1; b != R_NilValue;
+                         b = CDR(b), i++) {
+                        if (ARGUSED(b) != 2 && TAG(b) != R_NilValue &&
+                            pmatch(TAG(f), TAG(b), seendots)) {
+                            return false;
+                        }
+                    }
                 }
             }
+            f = CDR(f);
+            a = CDR(a);
+            arg_i++;
+        }
 
-        if (last != R_NilValue) {
-            UNPROTECT(protectedVars);
-            return false;
+        /* Third pass: matches based on order */
+        /* All args specified in tag=value form */
+        /* have now been matched.  If we find ... */
+        /* we gobble up all the remaining args. */
+        /* Otherwise we bind untagged values in */
+        /* order to any unmatched formals. */
+
+        f = formals;
+        a = actuals;
+        b = supplied;
+        seendots = FALSE;
+
+        while (f != R_NilValue && b != R_NilValue && !seendots) {
+            if (TAG(f) == R_DotsSymbol) {
+                /* Skip ... matching until all tags done */
+                seendots = TRUE;
+                f = CDR(f);
+                a = CDR(a);
+            } else if (CAR(a) != R_MissingArg) {
+                /* Already matched by tag */
+                /* skip to next formal */
+                f = CDR(f);
+                a = CDR(a);
+            } else if (ARGUSED(b) || TAG(b) != R_NilValue) {
+                /* This value used or tagged , skip to next value */
+                /* The second test above is needed because we */
+                /* shouldn't consider tagged values for positional */
+                /* matches. */
+                /* The formal being considered remains the same */
+                b = CDR(b);
+            } else {
+                /* We have a positional match */
+                SETCAR(a, CAR(b));
+                if (CAR(b) != R_MissingArg)
+                    SET_MISSING(a, 0);
+                SET_ARGUSED(b, 1);
+                b = CDR(b);
+                f = CDR(f);
+                a = CDR(a);
+            }
+        }
+
+        if (dots != R_NilValue) {
+            /* Gobble up all unused actuals */
+            SET_MISSING(dots, 0);
+            i = 0;
+            for (a = supplied; a != R_NilValue; a = CDR(a))
+                if (!ARGUSED(a))
+                    i++;
+
+            if (i) {
+                a = allocList(i);
+                SET_TYPEOF(a, DOTSXP);
+                f = a;
+                for (b = supplied; b != R_NilValue; b = CDR(b))
+                    if (!ARGUSED(b)) {
+                        SETCAR(f, CAR(b));
+                        SET_TAG(f, TAG(b));
+                        f = CDR(f);
+                    }
+                SETCAR(dots, a);
+            }
+        } else {
+            /* Check that all arguments are used */
+            for (b = supplied; b != R_NilValue; b = CDR(b))
+                if (!ARGUSED(b))
+                    return false;
         }
     }
-    UNPROTECT(protectedVars);
 
     // End of copy/paste snippet. Collecting results.
 
