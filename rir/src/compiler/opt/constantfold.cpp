911a42eea9865ad711dabe6e80e501492aaa168b
oli <o@o1o.ch>
2021-02-17 08:33:20 +0000

    refactoring

--- rir/src/compiler/opt/constantfold.cpp
+++ rir/src/compiler/opt/constantfold.cpp
@@ -15,8 +15,10 @@
 #include <iterator>
 #include <list>
 #include <unordered_set>
-namespace {
-using namespace rir::pir;
+
+namespace rir {
+namespace pir {
+
 static SEXP isConst(Value* instr) {
     instr = instr->followCastsAndForce();
 
@@ -149,9 +151,30 @@ static bool isStaticallyNA(Value* i) {
     return false;
 }
 
-} // namespace
-namespace rir {
-namespace pir {
+static long isStaticForceAndCall(Call* call) {
+    if (auto c = LdConst::Cast(call->cls())) {
+        if (TYPEOF(c->c()) == SPECIALSXP &&
+            c->c()->u.primsxp.offset == blt("forceAndCall") &&
+            call->nCallArgs() >= 2) {
+            if (auto n = LdConst::Cast(call->callArg(0).val())) {
+                if (TYPEOF(n->c()) == INTSXP || TYPEOF(n->c()) == REALSXP) {
+                    unsigned nForce = 0;
+                    if (Rf_length(n->c()) > 0) {
+                        if (TYPEOF(n->c()) == INTSXP) {
+                            auto t = INTEGER(n->c())[0];
+                            nForce = t < 0 || t == NA_INTEGER ? 0 : t;
+                        } else {
+                            auto t = REAL(n->c())[0];
+                            nForce = t < 0 || t != t ? 0 : t;
+                        }
+                    }
+                    return nForce;
+                }
+            }
+        }
+    }
+    return -1;
+}
 
 bool Constantfold::apply(Compiler& cmp, ClosureVersion* cls, Code* code,
                          LogStream&) const {
@@ -566,77 +589,46 @@ bool Constantfold::apply(Compiler& cmp, ClosureVersion* cls, Code* code,
                 // Constantfold forceAndCall if the n parameter is statically
                 // known
                 if (auto call = Call::Cast(i)) {
-                    if (auto c = LdConst::Cast(call->cls())) {
-                        if (TYPEOF(c->c()) == SPECIALSXP &&
-                            c->c()->u.primsxp.offset == blt("forceAndCall") &&
-                            call->nCallArgs() >= 2) {
-                            if (auto n =
-                                    LdConst::Cast(call->callArg(0).val())) {
-                                if (TYPEOF(n->c()) == INTSXP ||
-                                    TYPEOF(n->c()) == REALSXP) {
-                                    unsigned nForce = 0;
-                                    if (Rf_length(n->c()) > 0) {
-                                        if (TYPEOF(n->c()) == INTSXP) {
-                                            auto t = INTEGER(n->c())[0];
-                                            nForce = t < 0 || t == NA_INTEGER
-                                                         ? 0
-                                                         : t;
-                                        } else {
-                                            auto t = REAL(n->c())[0];
-                                            nForce = t < 0 || t != t ? 0 : t;
-                                        }
-                                    }
+                    auto nForce = isStaticForceAndCall(call);
+                    if (nForce != -1) {
 
-                                    bool nodots = true;
-                                    for (unsigned i = 2;
-                                         i < call->nCallArgs() &&
-                                         i <= nForce + 2;
-                                         ++i) {
-                                        if (call->callArg(i).val()->type.isA(
-                                                RType::expandedDots)) {
-                                            nodots = false;
-                                        }
-                                    }
+                        bool nodots = true;
+                        for (unsigned i = 2;
+                             i < call->nCallArgs() && i <= nForce + 2; ++i) {
+                            if (call->callArg(i).val()->type.isA(
+                                    RType::expandedDots)) {
+                                nodots = false;
+                            }
+                        }
 
-                                    if (nodots) {
-                                        ip = bb->insert(
-                                            ip, new ChkClosure(
-                                                    call->callArg(1).val()));
-                                        auto callee = *ip;
-                                        ip++;
-                                        std::vector<Value*> args;
-                                        for (unsigned i = 2;
-                                             i < call->nCallArgs(); ++i) {
-                                            if (i - 2 <= nForce) {
-                                                ip = bb->insert(
-                                                    ip,
-                                                    new CastType(
-                                                        call->callArg(i).val(),
-                                                        CastType::Upcast,
-                                                        RType::prom,
-                                                        PirType::any()));
-                                                ip = bb->insert(
-                                                    ip + 1,
-                                                    new Force(
-                                                        *ip, call->env(),
-                                                        Tombstone::
-                                                            framestate()));
-                                                args.push_back(*ip);
-                                                ip++;
-                                            } else {
-                                                args.push_back(
-                                                    call->callArg(i).val());
-                                            }
-                                        }
-                                        auto newCall = new Call(
-                                            call->env(), callee, args,
-                                            call->frameState(), call->srcIdx);
-                                        call->replaceUsesAndSwapWith(newCall,
-                                                                     ip);
-                                        next = ip + 1;
-                                    }
+                        if (nodots) {
+                            ip = bb->insert(
+                                ip, new ChkClosure(call->callArg(1).val()));
+                            auto callee = *ip;
+                            ip++;
+                            std::vector<Value*> args;
+                            for (unsigned i = 2; i < call->nCallArgs(); ++i) {
+                                if (i - 2 <= nForce) {
+                                    ip = bb->insert(
+                                        ip, new CastType(call->callArg(i).val(),
+                                                         CastType::Upcast,
+                                                         RType::prom,
+                                                         PirType::any()));
+                                    ip = bb->insert(
+                                        ip + 1,
+                                        new Force(*ip, call->env(),
+                                                  Tombstone::framestate()));
+                                    args.push_back(*ip);
+                                    ip++;
+                                } else {
+                                    args.push_back(call->callArg(i).val());
                                 }
                             }
+                            auto newCall =
+                                new Call(call->env(), callee, args,
+                                         call->frameState(), call->srcIdx);
+                            call->replaceUsesAndSwapWith(newCall, ip);
+                            next = ip + 1;
                         }
                     }
                 }
