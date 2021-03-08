7ccbd0d988faec7bf8f332fef326ebd47fbaadde
Sebastian Krynski <skrynski@gmail.com>
2021-03-08 18:42:07 +0000

    baseline disabled annotations

--- rir/src/api.cpp
+++ rir/src/api.cpp
@@ -162,17 +162,18 @@ REXPORT SEXP rirMarkFunction(SEXP what, SEXP which, SEXP reopt_,
             fun->flags.reset(Function::DisableNumArgumentsSpezialization);
     }
 
-    // bool DISABLE_ANNOTATIONS = getenv("PIR_DISABLE_ANNOTATIONS") ? true :
-    // false; if (!DISABLE_ANNOTATIONS) {
-    if (depromisedArgs != NA_LOGICAL) {
-        if (depromisedArgs)
-            fun->flags.set(Function::DepromisedArgs);
-        else
-            fun->flags.reset(Function::DepromisedArgs);
+    bool DISABLE_ANNOTATIONS = getenv("PIR_DISABLE_ANNOTATIONS") ? true : false;
+    DISABLE_ANNOTATIONS = true;
+    if (!DISABLE_ANNOTATIONS) {
+        if (depromisedArgs != NA_LOGICAL) {
+            if (depromisedArgs)
+                fun->flags.set(Function::DepromisedArgs);
+            else
+                fun->flags.reset(Function::DepromisedArgs);
         }
-        //}
+    }
 
-        return R_NilValue;
+    return R_NilValue;
 }
 
 REXPORT SEXP rirFunctionVersions(SEXP what) {
