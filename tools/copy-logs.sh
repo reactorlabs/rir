0b4bee0290edaabc3b8d6e1968cfb439baed655f
o <o@o1o.ch>
2021-05-26 21:02:30 +0200

    fix exporting failed test logs as build artifacts

---
+++ tools/copy-logs.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+TARGET=$2
+FILES=$(find $1 -iname '*.rout' -o -iname '*.log')
+
+mkdir -p $TARGET
+for f in $FILES; do
+  T="${TARGET}/$(echo $f | sed 's/\//-/g')"
+  cp $f $T
+done
+
+# This collects errors on failure and CI should fail afterwards
+exit 1
