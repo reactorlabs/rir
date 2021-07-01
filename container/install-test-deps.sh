3759711b4b506e96c548911d8c21bc858642d269
oli <o@o1o.ch>
2021-07-01 15:54:45 +0000

    fix deps

--- container/install-test-deps.sh
+++ container/install-test-deps.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get -o dir::cache::archives=apt-cache install -y -qq texlive-base xvfb
+DEBIAN_FRONTEND=noninteractive apt-get -o dir::cache::archives=apt-cache install -y -qq texlive-latex-base xvfb
