24c722df465868b0b2e399f6b50b84be55bcc949
oli <o@o1o.ch>
2021-07-02 09:07:06 +0000

    trying again with cache

--- container/install-test-deps.sh
+++ container/install-test-deps.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get -o dir::cache::archives=apt-cache install -y -qq texlive-latex-base xvfb texlive-fonts-extra tcl tk
+DEBIAN_FRONTEND=noninteractive apt-get -o dir::cache::archives=/apt-cache install -y -qq texlive-latex-base xvfb texlive-fonts-extra tcl tk
