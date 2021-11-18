cebaf12e3a1c10b6c517e869aeebadfe27d3ae34
oli <o@o1o.ch>
2021-11-18 18:24:33 +0000

    remove typefeedback contradicting what is in env

--- rir/src/compiler/opt/typefeedback_cleanup.cpp
+++ rir/src/compiler/opt/typefeedback_cleanup.cpp
@@ -24,40 +24,86 @@ bool TypefeedbackCleanup::apply(Compiler& cmp, ClosureVersion* cls, Code* code,
     TypeFeedback changedVarType;
 
     std::unordered_set<Instruction*> affected;
-    if (deoptCtx)
-        Visitor::run(version->entry, [&](Instruction* i) {
-            if (!i->hasTypeFeedback())
-                return;
-            if (i->typeFeedback().feedbackOrigin.pc() ==
-                deoptCtx->reason().pc()) {
-                if (deoptCtx->reason().reason == DeoptReason::Typecheck) {
-                    i->updateTypeFeedback().type = deoptCtx->typeCheckTrigger();
-                } else if (deoptCtx->reason().reason ==
-                           DeoptReason::DeadBranchReached) {
-                    if (deoptCtx->deadBranchTrigger() == R_TrueValue)
-                        i->updateTypeFeedback().value = True::instance();
-                    else if (deoptCtx->deadBranchTrigger() == R_FalseValue)
-                        i->updateTypeFeedback().value = False::instance();
-                } else if (deoptCtx->reason().reason ==
-                               DeoptReason::CallTarget ||
-                           deoptCtx->reason().reason == DeoptReason::DeadCall) {
-                    // TODO
-                }
-                if (auto ld = LdVar::Cast(i->followCastsAndForce())) {
-                    changedVar = ld->varName;
-                    changedVarType = i->typeFeedback();
-                }
-                affected.insert(i);
-            }
-            // If typefeedback contradicts actual type of deoptless continuation
-            // state then it is clearly stale
-            if (LdArg::Cast(i))
-                if (!i->typeFeedback().type.isVoid() &&
-                    (i->typeFeedback().type & i->type).isVoid()) {
-                    i->typeFeedback_->type = PirType::voyd();
+    if (deoptCtx) {
+        if (deoptCtx->reason().srcCode() != cls->rirSrc()) {
+            Visitor::run(version->entry, [&](Instruction* i) {
+                if (!i->hasTypeFeedback())
+                    return;
+                i->updateTypeFeedback().type = PirType::voyd();
+            });
+        } else {
+            Visitor::run(version->entry, [&](Instruction* i) {
+                if (!i->hasTypeFeedback())
+                    return;
+
+                if (i->typeFeedback().feedbackOrigin.pc() ==
+                    deoptCtx->reason().pc()) {
+                    if (deoptCtx->reason().reason == DeoptReason::Typecheck) {
+                        i->updateTypeFeedback().type =
+                            deoptCtx->typeCheckTrigger();
+                    } else if (deoptCtx->reason().reason ==
+                               DeoptReason::DeadBranchReached) {
+                        if (deoptCtx->deadBranchTrigger() == R_TrueValue)
+                            i->updateTypeFeedback().value = True::instance();
+                        else if (deoptCtx->deadBranchTrigger() == R_FalseValue)
+                            i->updateTypeFeedback().value = False::instance();
+                    } else if (deoptCtx->reason().reason ==
+                                   DeoptReason::CallTarget ||
+                               deoptCtx->reason().reason ==
+                                   DeoptReason::DeadCall) {
+                        // TODO
+                    }
+                    if (auto ld = LdVar::Cast(i->followCastsAndForce())) {
+                        changedVar = ld->varName;
+                        changedVarType = i->typeFeedback();
+                    }
                     affected.insert(i);
                 }
-        });
+                // If typefeedback contradicts actual type of deoptless
+                // continuation state then it is clearly stale
+                if (LdArg::Cast(i))
+                    if (!i->typeFeedback().type.isVoid() &&
+                        (i->typeFeedback().type & i->type).isVoid()) {
+                        i->typeFeedback_->type = PirType::voyd();
+                        affected.insert(i);
+                    }
+
+                // Update typefeedback with actual variable types in the env
+                auto trg = i->followCastsAndForce();
+                switch (i->tag) {
+                case Tag::Extract1_1D:
+                case Tag::Extract2_1D:
+                case Tag::Extract1_2D:
+                case Tag::Extract2_2D:
+                case Tag::Extract1_3D:
+                    trg = i->arg(0).val()->followCastsAndForce();
+                    break;
+                default: {
+                }
+                }
+                if (auto ld = LdVar::Cast(trg)) {
+                    if (!i->typeFeedback().type.isVoid() &&
+                        ld->varName != changedVar)
+                        for (auto v = deoptCtx->envBegin();
+                             v != deoptCtx->envEnd(); ++v)
+                            if (std::get<SEXP>(*v) == ld->varName)
+                                if (std::get<PirType>(*v) != RType::unbound &&
+                                    (std::get<PirType>(*v) &
+                                     i->typeFeedback().type)
+                                        .isVoid()) {
+                                    Rf_PrintValue(ld->varName);
+                                    i->updateTypeFeedback().type.print(
+                                        std::cout);
+                                    i->updateTypeFeedback().type =
+                                        std::get<PirType>(*v);
+                                    i->updateTypeFeedback().type.print(
+                                        std::cout);
+                                    affected.insert(i);
+                                }
+                }
+            });
+        }
+    }
 
     std::unordered_set<SEXP> otherAffectedVars;
     bool changed = true;
@@ -67,8 +113,15 @@ bool TypefeedbackCleanup::apply(Compiler& cmp, ClosureVersion* cls, Code* code,
             if (affected.count(i))
                 return;
             bool needUpdate = false;
-            if (auto ld = LdVar::Cast(i->followCastsAndForce())) {
-                if (ld->varName == changedVar) {
+            SEXP varName = nullptr;
+            auto trg = i->followCastsAndForce();
+            if (auto mk = MkArg::Cast(trg)) {
+                varName = mk->prom()->rirSrc()->trivialExpr;
+            } else if (auto ld = LdVar::Cast(trg)) {
+                varName = ld->varName;
+            }
+            if (varName) {
+                if (varName == changedVar) {
                     affected.insert(i);
                     i->updateTypeFeedback() = changedVarType;
                     if (Force::Cast(i))
@@ -76,7 +129,7 @@ bool TypefeedbackCleanup::apply(Compiler& cmp, ClosureVersion* cls, Code* code,
                             i->typeFeedback().type.forced();
                     changed = true;
                     return;
-                } else if (otherAffectedVars.count(ld->varName)) {
+                } else if (otherAffectedVars.count(varName)) {
                     needUpdate = true;
                 }
             }
