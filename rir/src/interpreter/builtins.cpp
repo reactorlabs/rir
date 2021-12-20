0e1602608495cb8cf7daa6eacaefbb8ce3330090
oli <o@o1o.ch>
2021-12-20 17:32:49 +0000

    fix forceAndCall issue
    builtins need unwrapped promises

--- rir/src/interpreter/builtins.cpp
+++ rir/src/interpreter/builtins.cpp
@@ -225,10 +225,13 @@ SEXP tryFastSpecialCall(CallContext& call, InterpreterInstance* ctx) {
         if (TYPEOF(fun) == BUILTINSXP || TYPEOF(fun) == CLOSXP) {
             for (int i = 0; i < n; i++) {
                 auto p = innerCall.stackArg(i);
-                if (TYPEOF(p) == PROMSXP)
-                    evaluatePromise(p);
-                else if (p == R_MissingArg)
+                if (TYPEOF(p) == PROMSXP) {
+                    auto v = evaluatePromise(p);
+                    if (TYPEOF(fun) == BUILTINSXP)
+                        innerCall.setStackArg(v, i);
+                } else if (p == R_MissingArg) {
                     Rf_errorcall(innerCall.ast, ("argument %d is empty"), i);
+                }
             }
         } else if (TYPEOF(fun) != SPECIALSXP) {
             Rf_error("attempt to apply non-function");
