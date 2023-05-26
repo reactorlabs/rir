af8b8e8a5a732f0320e7d6b1ca2b41a45e99c0df
jakobeha <jakobeha@gmail.com>
2023-05-25 23:53:30 -0400

    add compiler-client/compiler-server shared code

--- rir/src/compiler_server_client_shared_utils.h
+++ rir/src/compiler_server_client_shared_utils.h
@@ -10,10 +10,16 @@
 
 namespace rir {
 
+const uint64_t PIR_COMPILE_MAGIC = 0x217A25432A462D4A;
+
 static std::string printClosureVersionForCompilerServerComparison(pir::ClosureVersion* version) {
     std::stringstream pir;
     version->print(pir::DebugStyle::Standard, pir, true, false);
     return pir.str();
 }
+
+/// Alternative to `throw error_t()` in zeromq, since we don't allow exceptions.
+/// Used by external/zeromq so it may be unused before setup.
+__attribute__((unused)) NORET void zeromq_error();
 
 } // namespace rir
