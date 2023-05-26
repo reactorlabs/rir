f1e44af0419c7a432e4f7172aa0e08f7952703c4
jakobeha <jakobeha@gmail.com>
2023-05-26 10:46:16 -0400

    improvements to error reporting

--- rir/src/compiler_server_client_shared_utils.cpp
+++ rir/src/compiler_server_client_shared_utils.cpp
@@ -3,12 +3,20 @@
 //
 
 #include "compiler_server_client_shared_utils.h"
+#include "compiler/log/debug.h"
 #include <zmq.h>
 
 namespace rir {
 
-__attribute__((unused)) NORET void zeromq_error() {
-    std::cerr << "zeromq error: " << zmq_strerror(zmq_errno()) << std::endl;
+std::string printClosureVersionForCompilerServerComparison(pir::ClosureVersion* version) {
+    std::stringstream pir;
+    version->print(pir::DebugStyle::Standard, pir, true, false);
+    return pir.str();
+}
+
+__attribute__((unused)) NORET void zeromq_error(const char* func) {
+    printCBacktrace();
+    std::cerr << "zeromq error in" << func << ": " << zmq_strerror(zmq_errno()) << std::endl;
     std::abort();
 }
 
--- rir/src/compiler_server_client_shared_utils.h
+++ rir/src/compiler_server_client_shared_utils.h
@@ -2,7 +2,6 @@
 // Created by Jakob Hain on 5/25/23.
 //
 
-#include "compiler/log/debug.h"
 #include "compiler/pir/closure_version.h"
 #include <string>
 
@@ -12,14 +11,10 @@ namespace rir {
 
 const uint64_t PIR_COMPILE_MAGIC = 0x217A25432A462D4A;
 
-static std::string printClosureVersionForCompilerServerComparison(pir::ClosureVersion* version) {
-    std::stringstream pir;
-    version->print(pir::DebugStyle::Standard, pir, true, false);
-    return pir.str();
-}
+std::string printClosureVersionForCompilerServerComparison(pir::ClosureVersion* version);
 
 /// Alternative to `throw error_t()` in zeromq, since we don't allow exceptions.
 /// Used by external/zeromq so it may be unused before setup.
-__attribute__((unused)) NORET void zeromq_error();
+__attribute__((unused)) NORET void zeromq_error(const char* func);
 
 } // namespace rir
