2dd5099eb86f078e4b90a8734180dacea71639d8
jakobeha <jakobeha@gmail.com>
2023-06-13 17:01:07 -0400

    bugfix for memoized hashed response + improved tests

--- tools/test-compiler-client.r
+++ tools/test-compiler-client.r
@@ -1,5 +1,9 @@
 # Small closure (pir_regression.R)
 f <- pir.compile(rir.compile(function(a) a(b=1, 2)))
+# Memoized
+f <- pir.compile(rir.compile(function(a) a(b=1, 2)))
+# Memoized again
+f <- pir.compile(rir.compile(function(a) a(b=1, 2)))
 
 # Another small closure with a promise
 foo <- function(x) {
@@ -61,6 +65,56 @@ mandelbrot <- function(size) {
     return (sum)
 }
 
+stopifnot(pir.check(mandelbrot, NoExternalCalls, NoPromise, warmup=function(f) {f(13);f(27)}))
+
+# Memoized
+mandelbrot <- function(size) {
+    size = size
+    sum = 0
+    byteAcc = 0
+    bitNum  = 0
+    y = 0
+    while (y < size) {
+      ci = (2.0 * y / size) - 1.0
+      x = 0
+      while (x < size) {
+        zr   = 0.0
+        zrzr = 0.0
+        zi   = 0.0
+        zizi = 0.0
+        cr = (2.0 * x / size) - 1.5
+        z = 0
+        notDone = TRUE
+        escape = 0
+        while (notDone && (z < 50)) {
+          zr = zrzr - zizi + cr
+          zi = 2.0 * zr * zi + ci
+          zrzr = zr * zr
+          zizi = zi * zi
+          if ((zrzr + zizi) > 4.0) {
+            notDone = FALSE
+            escape  = 1
+          }
+          z = z + 1
+        }
+        byteAcc = bitwShiftL(byteAcc, 1) + escape
+        bitNum = bitNum + 1
+        if (bitNum == 8) {
+          sum = bitwXor(sum, byteAcc)
+          byteAcc = 0
+          bitNum  = 0
+        } else if (x == (size - 1)) {
+          byteAcc = bitwShiftL(byteAcc, 8 - bitNum)
+          sum = bitwXor(sum, byteAcc)
+          byteAcc = 0
+          bitNum  = 0
+        }
+        x = x + 1
+      }
+      y = y + 1
+    }
+    return (sum)
+}
 stopifnot(pir.check(mandelbrot, NoExternalCalls, NoPromise, warmup=function(f) {f(13);f(27)}))
 
 # Many closures (pir_regression6.R)
