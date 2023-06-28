59e969d1cf366811a393a219b62603cf8cf39304
jakobeha <jakobeha@gmail.com>
2023-06-28 10:04:33 -0400

    @WIP interning

--- rir/src/hash/UUIDPool.cpp
+++ rir/src/hash/UUIDPool.cpp
@@ -9,20 +9,88 @@
 #include "interpreter/serialize.h"
 #include <queue>
 
+#define LOG(stmt) stmt
+
 namespace rir {
 
 std::unordered_map<UUID, SEXP> UUIDPool::interned;
 std::unordered_map<SEXP, UUID> UUIDPool::hashes;
+std::unordered_map<SEXP, SEXP> UUIDPool::nextToIntern;
+std::unordered_map<SEXP, SEXP> UUIDPool::prevToIntern;
 std::unordered_set<SEXP> UUIDPool::preserved;
 std::unordered_map<UUID, ByteBuffer> UUIDPool::serialized;
 
 #ifdef DO_INTERN
+static void registerFinalizerIfPossible(SEXP e, R_CFinalizer_t finalizer) {
+    switch (TYPEOF(e)) {
+    case NILSXP:
+    case ENVSXP:
+    case EXTPTRSXP:
+    case BCODESXP:
+    case EXTERNALSXP:
+        R_RegisterCFinalizerEx(e, finalizer, (Rboolean) true);
+        break;
+    default:
+        // can't register finalizer, assume these don't get gcd
+        break;
+    }
+
+}
+
 void UUIDPool::uninternGcd(SEXP e) {
-    assert(!preserved.count(e));
+    assert(
+        !preserved.count(e) &&
+        "SEXP should not be preserved if it's getting uninterned because it was gcd?"
+    );
+
+    // Remove hash
+    assert(hashes.count(e) && "SEXP was never interned");
     auto hash = hashes.at(e);
-    interned.erase(hash);
-    serialized.erase(hash);
     hashes.erase(e);
+    assert(interned.count(hash) && "SEXP was interned, but the corresponding UUID is empty");
+
+    // Remove from the intern list for this UUID. If this is the first entry,
+    // update the interned UUID to point to the next SEXP. If there is no next,
+    // erase the interned UUID since there are no live SEXPs with that hash
+    // anymore.
+    if (prevToIntern.count(e)) {
+        // This isn't the first entry in the list with this UUID
+
+        // Linked list intermediate removal algorithm
+        auto prev = prevToIntern.at(e);
+        prevToIntern.erase(e);
+        assert(nextToIntern.count(prev) && nextToIntern.at(prev) == e);
+        if (nextToIntern.count(e)) {
+            auto next = nextToIntern.at(e);
+            nextToIntern.erase(e);
+            assert(prevToIntern.count(next) && prevToIntern.at(next) == e);
+            nextToIntern.at(prev) = next;
+            prevToIntern.at(next) = prev;
+        } else {
+            nextToIntern.erase(prev);
+        }
+        LOG(std::cout << "GC intern: " << hash << " -> " << e << "\n");
+    } else if (nextToIntern.count(e)) {
+        // This is the first entry in the list with this UUID, and there is
+        // another entry
+
+        // Linked list head removal algorithm
+        auto next = nextToIntern.at(e);
+        nextToIntern.erase(e);
+        assert(prevToIntern.count(next) && prevToIntern.at(next) == e);
+        prevToIntern.erase(next);
+
+        // Replace interned at UUID with the next SEXP
+        interned.at(hash) = next;
+        LOG(std::cout << "Switch intern: " << hash << " -> was " << e << " now " << next << "\n");
+    } else {
+        // This is the first and only entry in the list with this UUID
+
+        // Erase interned at UUID
+        interned.erase(hash);
+        serialized.erase(hash);
+        LOG(std::cout << "Remove intern: " << hash << " -> " << e << "\n");
+    }
 }
 #endif
 
@@ -32,30 +100,35 @@ SEXP UUIDPool::intern(SEXP e, const UUID& hash, bool preserve) {
     SLOWASSERT(hashSexp(e) == hash && "SEXP hash isn't deterministic or `hash` in `UUIDPool::intern(e, hash)` is wrong");
     UNPROTECT(1);
     if (interned.count(hash)) {
-        auto sexp = interned.at(hash);
-        if (preserve && !preserved.count(sexp)) {
-            R_PreserveObject(sexp);
-            preserved.insert(sexp);
+        auto existing = interned.at(hash);
+        if (!hashes.count(e)) {
+            LOG(std::cout << "Reuse intern: " << hash << " -> " << e << "\n");
+            hashes[e] = hash;
+
+            // Add to intern list for this UUID
+            auto oldLast = existing;
+            while (nextToIntern.count(oldLast)) {
+                oldLast = nextToIntern.at(oldLast);
+            }
+            nextToIntern[oldLast] = e;
+            prevToIntern[e] = oldLast;
+
+            registerFinalizerIfPossible(e, uninternGcd);
         }
-        return sexp;
+        e = existing;
+        if (preserve && !preserved.count(e)) {
+            R_PreserveObject(e);
+            preserved.insert(e);
+        }
+        return e;
     }
     if (preserve) {
         R_PreserveObject(e);
         preserved.insert(e);
     } else {
-        switch (TYPEOF(e)) {
-        case NILSXP:
-        case ENVSXP:
-        case EXTPTRSXP:
-        case BCODESXP:
-        case EXTERNALSXP:
-            R_RegisterCFinalizerEx(e, uninternGcd, (Rboolean) true);
-            break;
-        default:
-            // can't register finalizer, hopefully these don't get gcd
-            break;
-        }
+        registerFinalizerIfPossible(e, uninternGcd);
     }
+    LOG(std::cout << "New intern: " << hash << " -> " << e << "\n");
     interned[hash] = e;
     hashes[e] = hash;
 #endif
@@ -64,6 +137,14 @@ SEXP UUIDPool::intern(SEXP e, const UUID& hash, bool preserve) {
 
 SEXP UUIDPool::intern(SEXP e, bool recursive, bool preserve) {
 #ifdef DO_INTERN
+    if (hashes.count(e) && !recursive) {
+        // Already interned, don't compute hash
+        if (preserve && !preserved.count(e)) {
+            R_PreserveObject(e);
+            preserved.insert(e);
+        }
+        return e;
+    }
     if (recursive) {
         std::queue<SEXP> worklist;
         auto ret = intern(e, hashSexp(e, worklist), preserve);
