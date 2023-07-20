a2cb4411063c548d505f74a257733781e5e96442
jakobeha <jakobeha@gmail.com>
2023-07-20 05:43:26 -0400

    increase protects and measure deserialization

--- rir/src/utils/measuring.h
+++ rir/src/utils/measuring.h
@@ -9,16 +9,28 @@ namespace rir {
 class Measuring {
     struct TimingEvent;
 
-    static TimingEvent* startTimingEvent(const std::string& name, SEXP associated);
-    static void stopTimingEvent(TimingEvent* timing, bool associatedIsInitialized);
+    static TimingEvent* startTimingEvent(const std::string& name);
+    static void stopTimingEvent(TimingEvent* timing, SEXP associated,
+                                bool associatedIsInitialized);
   public:
+    static inline SEXP timeEvent(const std::string& name,
+                                 const std::function<SEXP()>& code,
+                                 const std::function<bool(SEXP associated)>&
+                                     associatedIsInitialized) {
+        auto timing = startTimingEvent(name);
+        auto associated = code();
+        PROTECT(associated);
+        auto isInitialized = associatedIsInitialized(associated);
+        stopTimingEvent(timing, associated, isInitialized);
+        UNPROTECT(1);
+        return associated;
+    }
     static inline void timeEvent(const std::string& name, SEXP associated,
-                                 bool associatedWillBeInitialized,
                                  const std::function<void()>& code) {
         PROTECT(associated);
-        auto timing = startTimingEvent(name, associated);
+        auto timing = startTimingEvent(name);
         code();
-        stopTimingEvent(timing, associatedWillBeInitialized);
+        stopTimingEvent(timing, associated, true);
         UNPROTECT(1);
     }
     template<typename T> static inline T
@@ -26,23 +38,32 @@ class Measuring {
               bool associatedWillBeInitialized,
               const std::function<T()>& code) {
         PROTECT(associated);
-        auto timing = startTimingEvent(name, associated);
+        auto timing = startTimingEvent(name);
         auto result = code();
-        stopTimingEvent(timing, associatedWillBeInitialized);
+        stopTimingEvent(timing, associated, associatedWillBeInitialized);
         UNPROTECT(1);
         return result;
     }
     template<typename T> static inline T
     timeEvent(const std::string& name, SEXP associated,
               const std::function<T()>& code) {
-        return timeEvent<T>(name, associated, true, code);
+        return timeEvent(name, associated, true, code);
+    }
+    static inline SEXP timeEventIf(bool cond, const std::string& name,
+                                   const std::function<SEXP()>& code,
+                                   const std::function<bool(SEXP associated)>&
+                                       associatedIsInitialized = [](SEXP _s){ return true; }) {
+        if (cond) {
+            return timeEvent(name, code, associatedIsInitialized);
+        } else {
+            return code();
+        }
     }
     static inline void timeEventIf(bool cond, const std::string& name,
                                    SEXP associated,
-                                   bool associatedWillBeInitialized,
                                    const std::function<void()>& code) {
         if (cond) {
-            timeEvent(name, associated, associatedWillBeInitialized, code);
+            timeEvent(name, associated, code);
         } else {
             code();
         }
@@ -57,15 +78,10 @@ class Measuring {
             return code();
         }
     }
-    static inline void timeEventIf(bool cond, const std::string& name,
-                                   SEXP associated,
-                                   const std::function<void()>& code) {
-        timeEventIf(cond, name, associated, true, code);
-    }
     template<typename T> static inline T
     timeEventIf(bool cond, const std::string& name, SEXP associated,
                 const std::function<T()>& code) {
-        return timeEventIf<T>(cond, name, associated, true, code);
+        return timeEventIf(cond, name, associated, true, code);
     }
 
     static void startTimer(const std::string& name);
