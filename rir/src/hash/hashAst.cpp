74a1a35ec358d7f71f2db27917c2bd5806f649a3
jakobeha <jakobeha@gmail.com>
2023-07-23 17:36:42 -0400

    measure hashing AST

--- rir/src/hash/hashAst.cpp
+++ rir/src/hash/hashAst.cpp
@@ -1,8 +1,10 @@
 #include "hashAst.h"
 #include "R/Funtab.h"
 #include "R/Symbols.h"
-#include <unordered_map>
+#include "compiler/parameter.h"
+#include "utils/measuring.h"
 #include <stack>
+#include <unordered_map>
 
 namespace rir {
 
@@ -35,194 +37,198 @@ using Stack = std::stack<Frame>;
 
 static void hashNewAst(SEXP s, UUID::Hasher& hasher,
                        std::function<void(SEXP)> recurse) {
-    // 2 fastcases below mean that every SEXP on the stack is not yet hashed,
-    // unless the symbol is a shared global, in which case it's trivial. So we
-    // don't bother checking if it's in hashCache
+    Measuring::timeEventIf(pir::Parameter::PIR_MEASURE_SERIALIZATION, "hashAst.cpp: hashNewAst", s, [&]{
+        // 2 fastcases below mean that every SEXP on the stack is not yet hashed,
+        // unless the symbol is a shared global, in which case it's trivial. So we
+        // don't bother checking if it's in hashCache
 
-    hasher.hashBytesOf<int>(TYPEOF(s));
-    switch (TYPEOF(s)) {
-    case NILSXP: {
-        break;
-    }
-
-    case SYMSXP: {
-        if (s == R_UnboundValue) {
-            hasher.hashBytesOf<int>(0);
-        } else if (s == R_MissingArg) {
-            hasher.hashBytesOf<int>(1);
-        } else if (s == R_RestartToken) {
-            hasher.hashBytesOf<int>(2);
-        } else if (s == symbol::expandDotsTrigger) {
-            hasher.hashBytesOf<int>(3);
-        } else {
-            hasher.hashBytesOf<int>(4);
-            const char* name = CHAR(PRINTNAME(s));
-            hasher.hashBytesOf<size_t>(strlen(name));
-            hasher.hashBytes((const void*)name, strlen(name));
+        hasher.hashBytesOf<int>(TYPEOF(s));
+        switch (TYPEOF(s)) {
+        case NILSXP: {
+            break;
         }
-        break;
-    }
 
-    case LISTSXP: {
-        hasher.hashBytesOf<int>(Rf_length(s));
-        for (SEXP cur = s; cur != R_NilValue; cur = CDR(cur)) {
-            recurse(CAR(cur));
+        case SYMSXP: {
+            if (s == R_UnboundValue) {
+                hasher.hashBytesOf<int>(0);
+            } else if (s == R_MissingArg) {
+                hasher.hashBytesOf<int>(1);
+            } else if (s == R_RestartToken) {
+                hasher.hashBytesOf<int>(2);
+            } else if (s == symbol::expandDotsTrigger) {
+                hasher.hashBytesOf<int>(3);
+            } else {
+                hasher.hashBytesOf<int>(4);
+                const char* name = CHAR(PRINTNAME(s));
+                hasher.hashBytesOf<size_t>(strlen(name));
+                hasher.hashBytes((const void*)name, strlen(name));
+            }
+            break;
         }
-        break;
-    }
 
-    case CLOSXP: {
-        assert(false && "unexpected CLOSXP in AST");
-    }
-
-    case ENVSXP: {
-        assert(false && "unexpected ENVSXP in AST");
-    }
-
-    case PROMSXP: {
-        assert(false && "unexpected PROMSXP in AST");
-    }
-
-    case LANGSXP: {
-        hasher.hashBytesOf<int>(Rf_length(s));
-        for (SEXP cur = s; cur != R_NilValue; cur = CDR(cur)) {
-            recurse(CAR(cur));
+        case LISTSXP: {
+            hasher.hashBytesOf<int>(Rf_length(s));
+            for (SEXP cur = s; cur != R_NilValue; cur = CDR(cur)) {
+                recurse(CAR(cur));
+            }
+            break;
         }
-        break;
-    }
 
-    case SPECIALSXP:
-    case BUILTINSXP: {
-        hasher.hashBytesOf<int>(getBuiltinNr(s));
-        break;
-    }
-
-    case CHARSXP: {
-        if (s == NA_STRING) {
-            hasher.hashBytesOf<int>(0);
-        } else {
-            hasher.hashBytesOf<int>(1);
-            const char* chr = CHAR(s);
-            hasher.hashBytesOf<size_t>(strlen(chr));
-            hasher.hashBytes((const void*)chr, strlen(chr));
+        case CLOSXP: {
+            assert(false && "unexpected CLOSXP in AST");
         }
-        break;
-    }
 
-    case LGLSXP: {
-        serializeAstVector(s, [&](int i) {
-            hasher.hashBytesOf<int>(LOGICAL(s)[i]);
-        });
-        break;
-    }
+        case ENVSXP: {
+            assert(false && "unexpected ENVSXP in AST");
+        }
 
-    case INTSXP: {
-        serializeAstVector(s, [&](int i) {
-            hasher.hashBytesOf<int>(INTEGER(s)[i]);
-        });
-        break;
-    }
+        case PROMSXP: {
+            assert(false && "unexpected PROMSXP in AST");
+        }
 
-    case REALSXP: {
-        serializeAstVector(s, [&](int i) {
-            hasher.hashBytesOf<double>(REAL(s)[i]);
-        });
-        break;
-    }
+        case LANGSXP: {
+            hasher.hashBytesOf<int>(Rf_length(s));
+            for (SEXP cur = s; cur != R_NilValue; cur = CDR(cur)) {
+                recurse(CAR(cur));
+            }
+            break;
+        }
 
-    case CPLXSXP: {
-        serializeAstVector(s, [&](int i) {
-            hasher.hashBytesOf<Rcomplex>(COMPLEX(s)[i]);
-        });
-        break;
-    }
+        case SPECIALSXP:
+        case BUILTINSXP: {
+            hasher.hashBytesOf<int>(getBuiltinNr(s));
+            break;
+        }
 
-    case STRSXP: {
-        serializeAstVector(s, [&](int i) {
-            const char* chr = CHAR(STRING_ELT(s, i));
-            hasher.hashBytesOf<size_t>(strlen(chr));
-            hasher.hashBytes((const void*)chr, strlen(chr));
-        });
-        break;
-    }
+        case CHARSXP: {
+            if (s == NA_STRING) {
+                hasher.hashBytesOf<int>(0);
+            } else {
+                hasher.hashBytesOf<int>(1);
+                const char* chr = CHAR(s);
+                hasher.hashBytesOf<size_t>(strlen(chr));
+                hasher.hashBytes((const void*)chr, strlen(chr));
+            }
+            break;
+        }
 
-    case VECSXP: {
-        serializeAstVector(s, [&](int i) {
-            recurse(VECTOR_ELT(s, i));
-        });
-        break;
-    }
+        case LGLSXP: {
+            serializeAstVector(s, [&](int i) {
+                hasher.hashBytesOf<int>(LOGICAL(s)[i]);
+            });
+            break;
+        }
 
-    case RAWSXP: {
-        serializeAstVector(s, [&](int i) {
-            hasher.hashBytesOf<Rbyte>(RAW(s)[i]);
-        });
-        break;
-    }
+        case INTSXP: {
+            serializeAstVector(s, [&](int i) {
+                hasher.hashBytesOf<int>(INTEGER(s)[i]);
+            });
+            break;
+        }
 
-    case EXTERNALSXP: {
-        assert(false && "unexpected RIR object in AST");
-    }
+        case REALSXP: {
+            serializeAstVector(s, [&](int i) {
+                hasher.hashBytesOf<double>(REAL(s)[i]);
+            });
+            break;
+        }
 
-    case DOTSXP:
-    case ANYSXP:
-    case EXPRSXP:
-    case BCODESXP:
-    case EXTPTRSXP:
-    case WEAKREFSXP:
-    case S4SXP:
-    case NEWSXP:
-    case FREESXP:
-    default: {
-        assert(false && "unexpected type in AST");
-    }
-    }
+        case CPLXSXP: {
+            serializeAstVector(s, [&](int i) {
+                hasher.hashBytesOf<Rcomplex>(COMPLEX(s)[i]);
+            });
+            break;
+        }
+
+        case STRSXP: {
+            serializeAstVector(s, [&](int i) {
+                const char* chr = CHAR(STRING_ELT(s, i));
+                hasher.hashBytesOf<size_t>(strlen(chr));
+                hasher.hashBytes((const void*)chr, strlen(chr));
+            });
+            break;
+        }
+
+        case VECSXP: {
+            serializeAstVector(s, [&](int i) {
+                recurse(VECTOR_ELT(s, i));
+            });
+            break;
+        }
+
+        case RAWSXP: {
+            serializeAstVector(s, [&](int i) {
+                hasher.hashBytesOf<Rbyte>(RAW(s)[i]);
+            });
+            break;
+        }
+
+        case EXTERNALSXP: {
+            assert(false && "unexpected RIR object in AST");
+        }
+
+        case DOTSXP:
+        case ANYSXP:
+        case EXPRSXP:
+        case BCODESXP:
+        case EXTPTRSXP:
+        case WEAKREFSXP:
+        case S4SXP:
+        case NEWSXP:
+        case FREESXP:
+        default: {
+            assert(false && "unexpected type in AST");
+        }
+        }
+    });
 }
 
 UUID hashAst(SEXP root) {
-    if (hashCache.count(root)) {
-        // Fastcase
-        return hashCache.at(root);
-    }
+    return Measuring::timeEventIf<UUID>(pir::Parameter::PIR_MEASURE_SERIALIZATION, "hashAst", root, [&]{
+        if (hashCache.count(root)) {
+            // Fastcase
+            return hashCache.at(root);
+        }
 
-    // Simulate a recursive call chain. Is this better or even as good letting
-    // the compiler do it? (There are a few differences in semantics from
-    // regular recursion which don't affect hash quality, like putting all SEXPs
-    // at the end)
-    Stack stack;
-    stack.emplace(root);
-    while (true) {
-        auto& top = stack.top();
-        // Hash this SEXP, changing the hasher and pushing not-started recursive
-        // calls onto the stack
-        top.started = true;
-        hashNewAst(top.sexp, top.hasher, [&](SEXP next){
-            if (hashCache.count(next)) {
-                // Fastcase
-                top.hasher.hashBytesOf<UUID>(hashCache.at(next));
-            } else {
-                stack.emplace(next);
-            }
-        });
+        // Simulate a recursive call chain. Is this better or even as good letting
+        // the compiler do it? (There are a few differences in semantics from
+        // regular recursion which don't affect hash quality, like putting all SEXPs
+        // at the end)
+        Stack stack;
+        stack.emplace(root);
+        while (true) {
+            auto& top = stack.top();
+            // Hash this SEXP, changing the hasher and pushing not-started recursive
+            // calls onto the stack
+            top.started = true;
+            hashNewAst(top.sexp, top.hasher, [&](SEXP next){
+                if (hashCache.count(next)) {
+                    // Fastcase
+                    top.hasher.hashBytesOf<UUID>(hashCache.at(next));
+                } else {
+                    stack.emplace(next);
+                }
+            });
 
-        // If this SEXP pushed not-started recursive calls we have to process
-        // them. If not, we can finish this call, and then finish outer calls
-        // which also have no more not-started recursive calls.
-        while (stack.top().started) {
-            auto sexp = stack.top().sexp;
-            auto hash = stack.top().hasher.finalize();
-            hashCache[sexp] = hash;
-            stack.pop();
-            if (stack.empty()) {
-                // Done
-                return hash;
-            } else {
-                // The SEXP's hash is part of the outer SEXP (whether it started
-                // or not)
-                stack.top().hasher.hashBytesOf<UUID>(hash);
+            // If this SEXP pushed not-started recursive calls we have to process
+            // them. If not, we can finish this call, and then finish outer calls
+            // which also have no more not-started recursive calls.
+            while (stack.top().started) {
+                auto sexp = stack.top().sexp;
+                auto hash = stack.top().hasher.finalize();
+                hashCache[sexp] = hash;
+                stack.pop();
+                if (stack.empty()) {
+                    // Done
+                    return hash;
+                } else {
+                    // The SEXP's hash is part of the outer SEXP (whether it started
+                    // or not)
+                    stack.top().hasher.hashBytesOf<UUID>(hash);
+                }
             }
         }
-    }
+    });
 }
 
 } // namespace rir
