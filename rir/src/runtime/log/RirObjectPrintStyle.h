0e90f5e191cad1a6d2914b2852a130f19c5edd45
jakobeha <jakobeha@gmail.com>
2023-07-26 10:08:01 -0400

    add logging code because it wasn't in lasst commit due to a bad global-gitignore

---
+++ rir/src/runtime/log/RirObjectPrintStyle.h
@@ -0,0 +1,24 @@
+//
+// Created by Jakob Hain on 7/26/23.
+//
+
+#pragma once
+
+namespace rir {
+
+#define LIST_OF_RIR_PRINT_STYLES(V) \
+    V(Default) \
+    V(Detailed) \
+    V(PrettyGraph) \
+
+/// Style to print RIR objects via `RirRuntimeObject::print`
+/// (`compiler/log/DebugStyle` is for PIR objects).
+enum class RirObjectPrintStyle {
+#define V(name) name,
+    LIST_OF_RIR_PRINT_STYLES(V)
+#undef V
+};
+
+extern RirObjectPrintStyle RIR_DEBUG_STYLE;
+
+} // namespace rir
