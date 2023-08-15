b3190211ee738bba0f78d510378f06f16b3fa358
Pierre Donat-Bouillud <pierre.donat.bouillud@gmail.com>
2023-08-15 15:12:27 +0000

    Dis Cleanup

--- rir/src/compiler/opt/pass_scheduler.cpp
+++ rir/src/compiler/opt/pass_scheduler.cpp
@@ -11,8 +11,8 @@ static std::regex getPassBlacklist() {
     auto filter = getenv("PIR_PASS_BLACKLIST");
     if (filter)
         return std::regex("");
-    // Filtering out ScopeResolution from all opt pipelines
-    return std::regex("ScopeResolution");
+    // Filtering out Cleanup from all opt pipelines
+    return std::regex("Cleanup");
 }
 
 static const std::regex PIR_PASS_BLACKLIST = getPassBlacklist();
