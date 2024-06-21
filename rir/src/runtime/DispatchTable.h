247dbe1cefb46f8ef360914e03f4b3b912e59567
Michal Štěpánek <161520055+stepam38@users.noreply.github.com>
2024-06-21 14:40:36 +0000

    MergingSmallerCandidates x TraversalFeedbackFilling performance measurement

--- rir/src/runtime/DispatchTable.h
+++ rir/src/runtime/DispatchTable.h
@@ -110,7 +110,7 @@ struct DispatchTable
                                                entry.second->copy()};
         PROTECT(p.second->container());
         MergeSmallerCandidatesMergingStrategy mergeStrategy(this);
-        TopFeedbackFillingStrategy fillStrategy;
+        TraversalFillingStrategy fillStrategy;
         p.second = mergeStrategy.merge(ctx, baseline(), p, feedbacks);
         p.second = fillStrategy.fill(ctx, p, feedbacks);
         UNPROTECT(1);
