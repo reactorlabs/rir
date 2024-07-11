c706f015dfa469045758c110e3a597d0b884d493
Filip Riha <riha.fina@gmail.com>
2024-07-11 16:36:33 +0000

    Dirty fix DeoptEvent

--- rir/src/recording.cpp
+++ rir/src/recording.cpp
@@ -365,12 +365,6 @@ void DeoptEvent::setTrigger(SEXP newTrigger) {
         triggerClosure_ = (ssize_t)rec;
         return;
     }
-
-    if (newTrigger) {
-        R_PreserveObject(newTrigger);
-    }
-
-    trigger_ = newTrigger;
 }
 
 const std::vector<const char*> DeoptEvent::fieldNames = {"funIdx",
