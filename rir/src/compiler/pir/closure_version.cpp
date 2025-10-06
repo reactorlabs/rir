ee85037a3be8a844fcba14f2c6cfb5cb0f8adee8
Sebastian Krynski <skrynski@gmail.com>
2025-10-06 13:56:47 +0000

    don't record  force->observed as used in the minimal setting

--- rir/src/compiler/pir/closure_version.cpp
+++ rir/src/compiler/pir/closure_version.cpp
@@ -185,7 +185,16 @@ void ClosureVersion::promiseInlined(Promise* promise) {
         .slotsPromiseInlined.insert(newSlots.begin(), newSlots.end());
 }
 
+const bool STATS_MINIMAL_USED =
+    std::getenv("STATS_MINIMAL_USED") != nullptr &&
+    std::string(std::getenv("STATS_MINIMAL_USED")) == "1";
+
 void ClosureVersion::registerProtoSlotUsedFromFO(const FeedbackOrigin& fo) {
+
+    // Only collect the used from final version and hoisted force
+    if (STATS_MINIMAL_USED) {
+        return;
+    }
 
     // Construct the slotUsed
     auto slotUsed = report::SlotUsed();
@@ -208,15 +217,8 @@ void ClosureVersion::registerProtoSlotUsedFromFO(const FeedbackOrigin& fo) {
     info.slotsUsed[fo.index()].push_back(slotUsed);
 }
 
-const bool STATS_MINIMAL_USED =
-    std::getenv("STATS_MINIMAL_USED") != nullptr &&
-    std::string(std::getenv("STATS_MINIMAL_USED")) == "1";
 
 void ClosureVersion::registerProtoSlotUsed(pir::Instruction* assume) {
-    // Only collect the used from final version and hoisted force
-    if (STATS_MINIMAL_USED) {
-        return;
-    }
 
     auto castedAssume = Assume::Cast(assume);
     assert(castedAssume && "Not an assume instruction");
