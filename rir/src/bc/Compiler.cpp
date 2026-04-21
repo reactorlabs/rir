7bb320a9752680bc42e3383a6352cfa6ecf535aa
Sebastian Krynski <skrynski@gmail.com>
2026-04-21 20:59:37 +0000

    recordless on

--- rir/src/bc/Compiler.cpp
+++ rir/src/bc/Compiler.cpp
@@ -2215,6 +2215,6 @@ bool Compiler::profile =
 
 bool Compiler::loopPeelingEnabled = true;
 
-bool Compiler::recordLessEnabled = false;
+bool Compiler::recordLessEnabled = true;
 
 } // namespace rir
