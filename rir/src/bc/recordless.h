996ee302d049d85a3ff7c4b8da0d9849655b1cc0
Sebastian Krynski <skrynski@gmail.com>
2026-06-04 14:17:43 +0000

    run again

--- rir/src/bc/recordless.h
+++ rir/src/bc/recordless.h
@@ -2,5 +2,5 @@
 
 #define RECORDLESS_EXPTREE_ENABLED // comment out to disable
 #ifdef RECORDLESS_EXPTREE_ENABLED
-//    #define RECORDLESS_EXPTREE_DEBUG // uncomment to enable debug prints
+//     #define RECORDLESS_EXPTREE_DEBUG // uncomment to enable debug prints
 #endif
